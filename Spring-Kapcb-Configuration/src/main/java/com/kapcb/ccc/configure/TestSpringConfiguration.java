11a8f1c7fdd280df3341f0d23638dc3cb4394b45
kapbc <2530628867@qq.com>
2020-12-25 17:28:28 +0800

    Spring @Configuration

---
+++ Spring-Kapcb-Configuration/src/main/java/com/kapcb/ccc/configure/TestSpringConfiguration.java
@@ -0,0 +1,21 @@
+package com.kapcb.ccc.configure;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * <a>Title: TestSpringConfiguration </a>
+ * <a>Author: kapcb <a>
+ * <a>Description: <a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2020/12/25 - 17:14
+ */
+@Configuration
+public class TestSpringConfiguration {
+
+    public TestSpringConfiguration(){
+        System.out.println("TestSpringConfiguration Content Begin To Start Up......");
+    }
+
+}
---
+++ Spring-Kapcb-Configuration/src/main/java/com/kapcb/ccc/tdd/TestConfig.java
@@ -0,0 +1,30 @@
+package com.kapcb.ccc.tdd;
+
+import com.kapcb.ccc.configure.TestSpringConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * <a>Title: TestConfig </a>
+ * <a>Author: kapcb <a>
+ * <a>Description: <a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2020/12/25 - 17:17
+ */
+public class TestConfig {
+
+    public static void main(String[] args) {
+
+        /**
+         * @Configuration 注解的 spring 容器加载方式，用AnnotationConfigApplicationContext替换ClassPathXmlApplicationContext
+         */
+        ApplicationContext context = new AnnotationConfigApplicationContext(TestSpringConfiguration.class);
+
+        /**
+         * 如果是加载xml配置文件则为
+         */
+        // ApplicationContext context = new ClassPathXmlApplicationContext("spring-context.xml");
+    }
+}
