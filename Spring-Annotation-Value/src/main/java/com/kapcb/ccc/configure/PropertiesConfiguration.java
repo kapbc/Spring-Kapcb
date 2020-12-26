7633be8e391c1cc638ca63aa16cf3b2d306140c0
kapbc <2530628867@qq.com>
2020-12-26 13:45:17 +0800

    Spring @Value

--- Spring-Annotation-Value/src/main/java/com/kapcb/ccc/configure/PropertiesConfiguration.java
+++ Spring-Annotation-Value/src/main/java/com/kapcb/ccc/configure/PropertiesConfiguration.java
@@ -1,14 +1,11 @@
 package com.kapcb.ccc.configure;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.Scope;
 import org.springframework.core.env.Environment;
 
-import javax.annotation.Resource;
 import javax.sql.DataSource;
 
 /**
