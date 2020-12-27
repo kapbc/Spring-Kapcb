7cc5c879358d4996a975f2288256a6e3edd7f3c5
kapbc <2530628867@qq.com>
2020-12-27 20:35:35 +0800

    Spring SpringMVC MyBatis

--- Spring-SpringMVC-MyBatis/src/main/java/com/kapcb/ccc/controller/SystemController.java
+++ Spring-SpringMVC-MyBatis/src/main/java/com/kapcb/ccc/controller/SystemController.java
@@ -1,5 +1,10 @@
 package com.kapcb.ccc.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 /**
  * <a>Title: SystemController </a>
  * <a>Author: kapcb <a>
@@ -9,5 +14,15 @@ package com.kapcb.ccc.controller;
  * @version 1.0.0
  * @date 2020/12/27 - 18:38
  */
+@Controller
+@RequestMapping("/test")
 public class SystemController {
+
+    @RequestMapping(value = "/getById", method = RequestMethod.GET)
+    public String test() {
+        Long id = 1L;
+        System.out.println("Spring MVC initial... success...");
+        return "success";
+    }
+
 }
--- Spring-SpringMVC-MyBatis/src/main/webapp/index.jsp
+++ Spring-SpringMVC-MyBatis/src/main/webapp/index.jsp
@@ -1,5 +1,6 @@
 <html>
 <body>
 <h2>Hello World!</h2>
+<a href="test/getById">test</a>
 </body>
 </html>
