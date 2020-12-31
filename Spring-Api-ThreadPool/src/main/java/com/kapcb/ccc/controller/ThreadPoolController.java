31903b1b3e0bef41ae4b0d6a421a996345d26822
Eircc <2530628867@qq.com>
2021-01-01 01:00:40 +0800

    Spring-Api-ThreadPool

--- Spring-Api-ThreadPool/src/main/java/com/kapcb/ccc/controller/ThreadPoolController.java
+++ Spring-Api-ThreadPool/src/main/java/com/kapcb/ccc/controller/ThreadPoolController.java
@@ -3,6 +3,7 @@ package com.kapcb.ccc.controller;
 import com.kapcb.ccc.domain.Person;
 import com.kapcb.ccc.service.IApiThreadPoolService;
 import lombok.RequiredArgsConstructor;
+import org.apache.log4j.Logger;
 import org.springframework.scheduling.concurrent.CustomizableThreadFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -34,11 +35,13 @@ import java.util.concurrent.atomic.AtomicReference;
 @RequestMapping(value = "/api/v1")
 public class ThreadPoolController {
 
+    private static final Logger logger = Logger.getLogger(ThreadPoolExecutor.class);
+
     private final IApiThreadPoolService apiThreadPoolService;
 
     @ResponseBody
     @PostMapping(value = "/getPersonById/{id}", produces = "application/json,charset=UTF-8")
-    public String execute(@PathVariable Long id) {
+    public void execute(@PathVariable Long id) {
         ThreadFactory threadFactory = new CustomizableThreadFactory("Kapcb-Thread-Pool-");
         ArrayBlockingQueue<Runnable> queue = new ArrayBlockingQueue<>(20);
         ThreadPoolExecutor executor = new ThreadPoolExecutor(5, 5, 500, TimeUnit.MILLISECONDS, queue, threadFactory);
@@ -49,20 +52,11 @@ public class ThreadPoolController {
                 result.set("success");
             }
         });
-
         try {
             future.get(5, TimeUnit.SECONDS);
+        } catch (InterruptedException | ExecutionException | TimeoutException e) {
+            logger.error("---Future Process Error---" + e.getMessage());
             future.cancel(true);
-
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        } catch (TimeoutException e) {
-            e.printStackTrace();
         }
-
-
     }
-
 }
