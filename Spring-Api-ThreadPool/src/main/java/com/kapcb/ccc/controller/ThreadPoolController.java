aaf6345fff0b34fd04bce9953289fd3bf92796f9
Eircc <2530628867@qq.com>
2021-01-05 23:06:10 +0800

    Spring-Api-ThreadPool

--- Spring-Api-ThreadPool/src/main/java/com/kapcb/ccc/controller/ThreadPoolController.java
+++ Spring-Api-ThreadPool/src/main/java/com/kapcb/ccc/controller/ThreadPoolController.java
@@ -6,8 +6,8 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.scheduling.concurrent.CustomizableThreadFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -39,8 +39,9 @@ public class ThreadPoolController {
     private final IApiThreadPoolService apiThreadPoolService;
 
     @ResponseBody
-    @PostMapping(path = "/getPersonById/{id}", produces = "application/json; charset=UTF-8")
+    @GetMapping(path = "/getPersonById/{id}", produces = "application/json; charset=UTF-8")
     public String execute(@PathVariable Long id) {
+        System.out.println("Id is ::: " + id);
         logger.warn("Path Variable is ::: " + id);
         ThreadFactory threadFactory = new CustomizableThreadFactory("Kapcb-Thread-Pool-");
         ArrayBlockingQueue<Runnable> queue = new ArrayBlockingQueue<>(20);
@@ -48,12 +49,13 @@ public class ThreadPoolController {
         logger.warn("---Begin to process---");
         Future<?> future = executor.submit(() -> apiThreadPoolService.getPersonById(id));
         try {
+            System.out.println("Begin to process");
             future.get(5, TimeUnit.SECONDS);
             logger.warn("---Process Success---");
         } catch (InterruptedException | ExecutionException | TimeoutException e) {
             logger.error("---Future Process Error---" + e.getMessage());
             future.cancel(true);
         }
-        return "process success";
+        return "---process success---";
     }
 }
--- Spring-Api/src/main/java/com/kapcb/ccc/controller/UserApiController.java
+++ Spring-Api/src/main/java/com/kapcb/ccc/controller/UserApiController.java
@@ -7,6 +7,7 @@ import com.kapcb.ccc.common.result.ResultInfo;
 import com.kapcb.ccc.domain.User;
 import com.kapcb.ccc.service.IUserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.scheduling.concurrent.CustomizableThreadFactory;
