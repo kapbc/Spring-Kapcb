d56daadca305e0c3bb2e581659fb83eea2f9b264
kapcb <55942617+kapbc@users.noreply.github.com>
2021-01-07 17:14:26 +0800

    Create HttpPostTest.java

---
+++ Spring-Utils/src/main/java/com/kapcb/ccc/tdd/HttpPostTest.java
@@ -0,0 +1,69 @@
+package com.kapcb.ccc.tdd;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * <a>Title: HttpPostTest </a>
+ * <a>Author: kapcb <a>
+ * <a>Description：<a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2021/1/7-17:01
+ */
+public class HttpPostTest {
+
+    private static final String REQUEST_URL = "";
+
+
+    private HttpPostTest() {
+    }
+
+    public static void post() {
+        CloseableHttpClient aDefault = HttpClients.createDefault();
+        HttpPost httpPost = new HttpPost(REQUEST_URL);
+        httpPost.setHeader("Connection", "keep-alive");
+        httpPost.setHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36");
+        httpPost.setHeader("Cookie", "UM_distinctid=16442706a09352-0376059833914f-3c604504-1fa400-16442706a0b345; CNZZDATA1262458286=1603637673-1530123020-%7C1530123020; JSESSIONID=805587506F1594AE02DC45845A7216A4");
+
+        List<BasicNameValuePair> params = new ArrayList<>();
+        params.add(new BasicNameValuePair("username", "123456"));
+
+        CloseableHttpResponse httpResponse = null;
+        try {
+            httpPost.setEntity(new UrlEncodedFormEntity(params, "UTF-8"));
+            httpResponse = aDefault.execute(httpPost);
+            HttpEntity entity = httpResponse.getEntity();
+            String result = EntityUtils.toString(entity);
+            System.out.println("result = " + result);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (aDefault != null) {
+                try {
+                    aDefault.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (httpResponse != null) {
+                try {
+                    httpResponse.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
