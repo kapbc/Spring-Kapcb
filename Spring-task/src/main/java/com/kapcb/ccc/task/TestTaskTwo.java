890367dc567c7a1784a8684ca647d33c713e7574
Eircc <2530628867@qq.com>
2021-01-07 19:53:32 +0800

    Spring-task

--- Spring-task/src/main/java/com/kapcb/ccc/task/TestTaskTwo.java
+++ Spring-task/src/main/java/com/kapcb/ccc/task/TestTaskTwo.java
@@ -22,7 +22,7 @@ public class TestTaskTwo {
 
     private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss SSS");
 
-    @Scheduled(cron = "0 * * * * ?")
+    @Scheduled(cron = "0/10 * * * * ?")
     public void testTaskTwo() {
         LocalDateTime now = LocalDateTime.now();
         String currentTime = now.format(DATE_TIME_FORMATTER);
