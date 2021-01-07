0f8d1d6a1948e69da6b2a946f108ae7435a81058
kapcb <55942617+kapbc@users.noreply.github.com>
2021-01-07 10:13:12 +0800

    TestTaskTwo

---
+++ Spring-task/src/main/java/com/kapcb/ccc/task/TestTaskTwo.java
@@ -0,0 +1,31 @@
+package com.kapcb.ccc.task;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * <a>Title: TestTaskTwo </a>
+ * <a>Author: kapcb <a>
+ * <a>Description：<a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2021/1/7-9:41
+ */
+@Slf4j
+@Component("taskJob")
+public class TestTaskTwo {
+
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss SSS");
+
+    @Scheduled(cron = "0 * * * * ?")
+    public void testTaskTwo() {
+        LocalDateTime now = LocalDateTime.now();
+        String currentTime = now.format(DATE_TIME_FORMATTER);
+        log.info("The Task Job Two Begin To Run, Current Local Time Is ::: " + currentTime);
+    }
+}
