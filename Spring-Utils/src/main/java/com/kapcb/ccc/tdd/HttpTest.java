d0f5c7d25ccac4d352d7d0a528e709021af79fb4
kapcb <55942617+kapbc@users.noreply.github.com>
2021-01-07 17:00:46 +0800

    Create HttpTest.java

---
+++ Spring-Utils/src/main/java/com/kapcb/ccc/tdd/HttpTest.java
@@ -0,0 +1,17 @@
+package com.kapcb.ccc.tdd;
+
+/**
+ * <a>Title: HttpTest </a>
+ * <a>Author: kapcb <a>
+ * <a>Description：<a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2021/1/7-16:40
+ */
+public class HttpTest {
+
+    public static void main(String[] args) {
+        HttpGetTest.get();
+    }
+}
