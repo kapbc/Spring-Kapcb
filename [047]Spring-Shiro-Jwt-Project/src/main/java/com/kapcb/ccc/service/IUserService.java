7a221701865dd08516a90ade392281c6ae25fde0
kapcb <55942617+kapbc@users.noreply.github.com>
2021-01-20 15:32:54 +0800

    Create IUserService.java

---
+++ [047]Spring-Shiro-Jwt-Project/src/main/java/com/kapcb/ccc/service/IUserService.java
@@ -0,0 +1,13 @@
+package com.kapcb.ccc.service;
+
+/**
+ * <a>Title: IUserService </a>
+ * <a>Author: kapcb <a>
+ * <a>Description：<a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2021/1/20-15:19
+ */
+public interface IUserService {
+}
