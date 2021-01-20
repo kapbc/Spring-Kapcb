19dbfa81082a3fad34d65f6901f54e29b203f021
kapcb <55942617+kapbc@users.noreply.github.com>
2021-01-20 15:33:13 +0800

    Create IUserServiceImpl.java

---
+++ [047]Spring-Shiro-Jwt-Project/src/main/java/com/kapcb/ccc/service/impl/IUserServiceImpl.java
@@ -0,0 +1,19 @@
+package com.kapcb.ccc.service.impl;
+
+import com.kapcb.ccc.service.IUserService;
+import org.springframework.stereotype.Service;
+
+/**
+ * <a>Title: IUserServiceImpl </a>
+ * <a>Author: kapcb <a>
+ * <a>Description：<a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2021/1/20-15:19
+ */
+@Service("userService")
+public class IUserServiceImpl implements IUserService {
+
+
+}
