979ac81f564e34db7f7272ebd287377f6fd7def9
kapcb <55942617+kapbc@users.noreply.github.com>
2021-01-20 15:30:57 +0800

    Create JwtToken.java

---
+++ [047]Spring-Shiro-Jwt-Project/src/main/java/com/kapcb/ccc/commons/jwt/JwtToken.java
@@ -0,0 +1,41 @@
+package com.kapcb.ccc.commons.jwt;
+
+import com.kapcb.ccc.commons.constants.Constant;
+import com.kapcb.ccc.commons.utils.JwtUtil;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.shiro.authc.AuthenticationToken;
+
+/**
+ * <a>Title: JwtToken </a>
+ * <a>Author: kapcb <a>
+ * <a>Description：<a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2021/1/20-14:17
+ */
+@Slf4j
+@Data
+@SuppressWarnings("serial")
+public class JwtToken implements AuthenticationToken {
+
+    private String token;
+    private String username;
+
+    public JwtToken(String token) {
+        this.token = token;
+        this.username = JwtUtil.getClaimFiled(token, Constant.JWT_USERNAME.getString());
+    }
+
+    @Override
+
+    public Object getPrincipal() {
+        return null;
+    }
+
+    @Override
+    public Object getCredentials() {
+        return null;
+    }
+}
