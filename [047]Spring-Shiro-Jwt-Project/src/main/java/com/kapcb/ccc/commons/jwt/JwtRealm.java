b33a09f74507c57e09666d7efcaf77fcff2c89f9
kapcb <55942617+kapbc@users.noreply.github.com>
2021-01-20 15:30:26 +0800

    Create JwtRealm.java

---
+++ [047]Spring-Shiro-Jwt-Project/src/main/java/com/kapcb/ccc/commons/jwt/JwtRealm.java
@@ -0,0 +1,35 @@
+package com.kapcb.ccc.commons.jwt;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.springframework.stereotype.Component;
+
+/**
+ * <a>Title: JwtRealm </a>
+ * <a>Author: kapcb <a>
+ * <a>Description：<a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2021/1/20-15:04
+ */
+@Slf4j
+@Component
+public class JwtRealm extends AuthorizingRealm {
+
+
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+        return null;
+    }
+
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        return null;
+    }
+}
