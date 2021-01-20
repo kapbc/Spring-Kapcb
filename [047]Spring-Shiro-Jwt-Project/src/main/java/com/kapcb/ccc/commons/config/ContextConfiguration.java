80072ff7302c957bf878d04417c7b1d91572351a
kapcb <55942617+kapbc@users.noreply.github.com>
2021-01-20 15:27:36 +0800

    Create ContextConfiguration.java

---
+++ [047]Spring-Shiro-Jwt-Project/src/main/java/com/kapcb/ccc/commons/config/ContextConfiguration.java
@@ -0,0 +1,18 @@
+package com.kapcb.ccc.commons.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * <a>Title: ContextConfiguration </a>
+ * <a>Author: kapcb <a>
+ * <a>Description：<a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2021/1/20-8:24
+ */
+@Slf4j
+@Configuration
+public class ContextConfiguration {
+}
