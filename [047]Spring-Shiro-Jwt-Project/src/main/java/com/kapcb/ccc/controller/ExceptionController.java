8a5651212315dab7a573fd5c29e27a9a15861d65
kapcb <55942617+kapbc@users.noreply.github.com>
2021-01-21 10:19:13 +0800

    Update ExceptionController.java

--- [047]Spring-Shiro-Jwt-Project/src/main/java/com/kapcb/ccc/controller/ExceptionController.java
+++ [047]Spring-Shiro-Jwt-Project/src/main/java/com/kapcb/ccc/controller/ExceptionController.java
@@ -1,5 +1,6 @@
 package com.kapcb.ccc.controller;
 
+import com.kapcb.ccc.commons.constants.Constant;
 import com.kapcb.ccc.commons.domain.Result;
 import com.kapcb.ccc.commons.utils.JsonUtil;
 import lombok.extern.slf4j.Slf4j;
@@ -24,13 +25,13 @@ public class ExceptionController {
     public String handlerShiroException(ShiroException e) {
         log.warn("handler shiro exception in exception controller");
         log.warn("the exception message is : " + e.getMessage());
-        return JsonUtil.convertToJsonString(new Result<>(e.getMessage(), "401"));
+        return JsonUtil.convertToJsonString(new Result<>(e.getMessage(), Constant.FOUR_ZERO_ONE.getString()));
     }
 
     @ExceptionHandler(value = Exception.class)
     public String handlerSystemException(Exception e) {
         log.warn("handler system exception in exception controller");
         log.warn("the exception message is : " + e.getMessage());
-        return JsonUtil.convertToJsonString(new Result<>(e.getMessage(), "400"));
+        return JsonUtil.convertToJsonString(new Result<>(e.getMessage(), Constant.FOUR_ZERO_ZEO.getString()));
     }
 }
