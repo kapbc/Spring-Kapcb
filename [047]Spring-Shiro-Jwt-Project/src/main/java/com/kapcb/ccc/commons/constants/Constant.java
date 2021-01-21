fab3f9af9a4e4be75ad30d5c9f2af4f904170830
kapcb <55942617+kapbc@users.noreply.github.com>
2021-01-21 10:18:23 +0800

    Update Constant.java

--- [047]Spring-Shiro-Jwt-Project/src/main/java/com/kapcb/ccc/commons/constants/Constant.java
+++ [047]Spring-Shiro-Jwt-Project/src/main/java/com/kapcb/ccc/commons/constants/Constant.java
@@ -12,22 +12,37 @@ package com.kapcb.ccc.commons.constants;
 public enum Constant {
 
     /**
-     * jwt constants
+     * JWT CONFIGURATION CONSTANTS
      */
     JWT_USERNAME("username", "constant of the jwt claim"),
     JWT_AUTHORIZATION_HEARD("X-Authorization-With", "the key of the jwt heard"),
     JWT_SECRET("kapcb", "the secret key of the jwt"),
     HTTP_RESPONSE_CONTENT_TYPE("application/json; charset = UTF-8", "http response content type"),
 
+    /**
+     * SHIRO CONFIGURATION CONSTANTS
+     */
     SHIRO_REALM_ALGORITHM("SHA-1", "the shiro encrypt algorithm"),
 
-    Y("Y", "user account flag"),
+    /**
+     * SYSTEM RESPONSE STATUS CODE
+     */
+    FOUR_ZERO_ONE(401, "the response status code 401"),
+    FOUR_ZERO_ZEO(400, "the response status code 400"),
 
+    /**
+     * SYSTEM CHARACTER CONSTANTS
+     */
+    Y("Y", "user account is locked flag"),
+
+    /**
+     * SYSTEM NUMBER CONSTANTS
+     */
     SIXTEEN(16, "constant of 16");
 
     private String stringCode;
     private int intCode;
-    private String description;
+    private final String description;
 
     Constant(String stringCode, String description) {
         this.stringCode = stringCode;
