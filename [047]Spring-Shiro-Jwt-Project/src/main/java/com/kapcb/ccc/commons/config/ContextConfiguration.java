55ec5a5f3996caebac6a462f60ebd27afddd83cb
kapcb <55942617+kapbc@users.noreply.github.com>
2021-01-21 08:46:07 +0800

    Update ContextConfiguration.java

--- [047]Spring-Shiro-Jwt-Project/src/main/java/com/kapcb/ccc/commons/config/ContextConfiguration.java
+++ [047]Spring-Shiro-Jwt-Project/src/main/java/com/kapcb/ccc/commons/config/ContextConfiguration.java
@@ -2,6 +2,7 @@ package com.kapcb.ccc.commons.config;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 /**
  * <a>Title: ContextConfiguration </a>
@@ -14,5 +15,6 @@ import org.springframework.context.annotation.Configuration;
  */
 @Slf4j
 @Configuration
+@Import(ShiroConfiguration.class)
 public class ContextConfiguration {
 }
