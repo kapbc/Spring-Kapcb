4fdf4cf4e5809b564aa3632d568954b63d438a3d
kapcb <55942617+kapbc@users.noreply.github.com>
2021-01-22 15:15:56 +0800

    Update JsonUtil.java

--- [049]SpringMVC-Shiro-Jwt/src/main/java/com/kapcb/ccc/commons/utils/json/JsonUtil.java
+++ [049]SpringMVC-Shiro-Jwt/src/main/java/com/kapcb/ccc/commons/utils/json/JsonUtil.java
@@ -1,5 +1,10 @@
 package com.kapcb.ccc.commons.utils.json;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kapcb.ccc.commons.constant.ResultInfo;
+import com.kapcb.ccc.commons.domain.Result;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -13,4 +18,16 @@ import lombok.extern.slf4j.Slf4j;
  */
 @Slf4j
 public class JsonUtil {
+
+    private JsonUtil() {
+    }
+
+    @SneakyThrows(JsonProcessingException.class)
+    public static <T> String convertResultToJsonString(Result<T> result) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        result = result == null ? new Result<>(ResultInfo.FAIL) : result;
+        String jsonString = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(result);
+        log.info("the convert json string is : " + jsonString);
+        return jsonString;
+    }
 }
