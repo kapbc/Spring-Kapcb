a7ae902a82bfb4e242b35cdb7068125c415e5093
kapcb <55942617+kapbc@users.noreply.github.com>
2021-01-22 09:45:47 +0800

    Create IPermissionServiceImpl.java

---
+++ [049]SpringMVC-Shiro-Jwt/src/main/java/com/kapcb/ccc/service/impl/IPermissionServiceImpl.java
@@ -0,0 +1,20 @@
+package com.kapcb.ccc.service.impl;
+
+import com.kapcb.ccc.service.IPermissionService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * <a>Title: IPermissionServiceImpl </a>
+ * <a>Author: kapcb <a>
+ * <a>Description：<a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2021/1/22-9:25
+ */
+@Slf4j
+@Service(value = "permissionService")
+public class IPermissionServiceImpl implements IPermissionService {
+
+}
