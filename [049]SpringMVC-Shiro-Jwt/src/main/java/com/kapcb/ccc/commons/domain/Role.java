28b6920bbac334d437d71c14ccab6fa52e7aa9d4
kapcb <55942617+kapbc@users.noreply.github.com>
2021-01-22 10:42:20 +0800

    Create Role.java

---
+++ [049]SpringMVC-Shiro-Jwt/src/main/java/com/kapcb/ccc/commons/domain/Role.java
@@ -0,0 +1,23 @@
+package com.kapcb.ccc.commons.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * <a>Title: Role </a>
+ * <a>Author: kapcb <a>
+ * <a>Description：<a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2021/1/22-9:50
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role {
+
+    private String username;
+    private String roles;
+}
