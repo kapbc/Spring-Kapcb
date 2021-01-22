24bc69b6a74e42cca9ecafd8f4d8ff1e2f905be4
kapcb <55942617+kapbc@users.noreply.github.com>
2021-01-22 09:18:35 +0800

    Create JsonUtil.java

---
+++ [049]SpringMVC-Shiro-Jwt/src/main/java/com/kapcb/ccc/commons/utils/json/JsonUtil.java
@@ -0,0 +1,16 @@
+package com.kapcb.ccc.commons.utils.json;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * <a>Title: JsonUtil </a>
+ * <a>Author: kapcb <a>
+ * <a>Description: <a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2021/1/21 22:54
+ */
+@Slf4j
+public class JsonUtil {
+}
