373516c303e5253530f48ee32e0a1987f0790cb5
kapcb <55942617+kapbc@users.noreply.github.com>
2021-01-22 09:46:49 +0800

    Create IRoleServiceImpl.java

---
+++ [049]SpringMVC-Shiro-Jwt/src/main/java/com/kapcb/ccc/service/impl/IRoleServiceImpl.java
@@ -0,0 +1,19 @@
+package com.kapcb.ccc.service.impl;
+
+import com.kapcb.ccc.service.IRoleService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * <a>Title: IRoleServiceImpl </a>
+ * <a>Author: kapcb <a>
+ * <a>Description：<a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2021/1/22-9:24
+ */
+@Slf4j
+@Service(value = "roleService")
+public class IRoleServiceImpl implements IRoleService {
+}
