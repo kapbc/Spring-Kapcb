ab1fd83e5ed8bebe914c03751e2d98814936d619
kapcb <55942617+kapbc@users.noreply.github.com>
2021-01-22 09:41:44 +0800

    Create IPermissionService.java

---
+++ [049]SpringMVC-Shiro-Jwt/src/main/java/com/kapcb/ccc/service/IPermissionService.java
@@ -0,0 +1,13 @@
+package com.kapcb.ccc.service;
+
+/**
+ * <a>Title: IPermissionService </a>
+ * <a>Author: kapcb <a>
+ * <a>Description：<a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2021/1/22-9:25
+ */
+public interface IPermissionService {
+}
