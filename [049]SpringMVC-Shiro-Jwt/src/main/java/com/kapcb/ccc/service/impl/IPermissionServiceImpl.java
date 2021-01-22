3214879e6662b80bc6d4383b521cd173019b3d83
kapcb <55942617+kapbc@users.noreply.github.com>
2021-01-22 11:21:21 +0800

    Update IPermissionServiceImpl.java

--- [049]SpringMVC-Shiro-Jwt/src/main/java/com/kapcb/ccc/service/impl/IPermissionServiceImpl.java
+++ [049]SpringMVC-Shiro-Jwt/src/main/java/com/kapcb/ccc/service/impl/IPermissionServiceImpl.java
@@ -1,8 +1,12 @@
 package com.kapcb.ccc.service.impl;
 
+import com.kapcb.ccc.commons.pool.DataPool;
 import com.kapcb.ccc.service.IPermissionService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * <a>Title: IPermissionServiceImpl </a>
@@ -17,4 +21,11 @@ import org.springframework.stereotype.Service;
 @Service(value = "permissionService")
 public class IPermissionServiceImpl implements IPermissionService {
 
+    @Override
+    public Set<String> getPermissionByUsername(String username) {
+        log.info("begin to process the get permission by username");
+        Set<String> permissionSet = DataPool.permissionMap.getOrDefault(username, new HashSet<>());
+        log.info("the permissions of " + username + " is : " + permissionSet);
+        return permissionSet;
+    }
 }
