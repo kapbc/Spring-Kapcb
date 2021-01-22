389e618f719426e965cb6696f7cc716dc9760da2
kapcb <55942617+kapbc@users.noreply.github.com>
2021-01-22 16:31:37 +0800

    Update IUserService.java

--- [049]SpringMVC-Shiro-Jwt/src/main/java/com/kapcb/ccc/service/IUserService.java
+++ [049]SpringMVC-Shiro-Jwt/src/main/java/com/kapcb/ccc/service/IUserService.java
@@ -2,6 +2,8 @@ package com.kapcb.ccc.service;
 
 import com.kapcb.ccc.commons.domain.User;
 
+import java.util.List;
+
 /**
  * <a>Title: IUserService </a>
  * <a>Author: kapcb <a>
@@ -20,4 +22,43 @@ public interface IUserService {
      * @return User
      */
     User getUserInfoByUsername(String username);
+
+    /**
+     * get user info
+     *
+     * @return List<User>
+     */
+    List<User> getUserInfo();
+
+    /**
+     * get user by user id
+     *
+     * @param id String
+     * @return User
+     */
+    User getUserById(String id);
+
+    /**
+     * delete user by id
+     *
+     * @param id String
+     * @return boolean
+     */
+    boolean deleteUserById(String id);
+
+    /**
+     * update user by id
+     *
+     * @param id String
+     * @return boolean
+     */
+    boolean updateUserById(String id);
+
+    /**
+     * insert user
+     *
+     * @param id String
+     * @return boolean
+     */
+    boolean insertUser(String id);
 }
