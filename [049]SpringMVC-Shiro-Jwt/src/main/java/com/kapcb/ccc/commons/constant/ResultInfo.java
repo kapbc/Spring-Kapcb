0dd9592f254c70da075ec577c6317ecf06008d4b
kapcb <55942617+kapbc@users.noreply.github.com>
2021-01-22 15:12:21 +0800

    Create ResultInfo.java

---
+++ [049]SpringMVC-Shiro-Jwt/src/main/java/com/kapcb/ccc/commons/constant/ResultInfo.java
@@ -0,0 +1,35 @@
+package com.kapcb.ccc.commons.constant;
+
+/**
+ * <a>Title: ResultInfo </a>
+ * <a>Author: kapcb <a>
+ * <a>Description：<a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2021/1/22-15:03
+ */
+public enum ResultInfo {
+
+    SUCCESS("success", "200"),
+    FAIL("fail", "201"),
+    INVALID("invalid request", "202"),
+    LOCKED("account locked", "203"),
+    UN_LOGIN("need to login", "204");
+
+    private String message;
+    private String code;
+
+    ResultInfo(String message, String code) {
+        this.message = message;
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+
+    public String getCode() {
+        return this.code;
+    }
+}
