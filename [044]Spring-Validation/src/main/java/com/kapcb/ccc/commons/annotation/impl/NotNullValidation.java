2c70f400e0b9d87aedf283f2a433b765681a59f3
kapcb <55942617+kapbc@users.noreply.github.com>
2021-01-25 17:27:30 +0800

    Update NotNullValidation.java

--- [044]Spring-Validation/src/main/java/com/kapcb/ccc/commons/annotation/impl/NotNullValidation.java
+++ [044]Spring-Validation/src/main/java/com/kapcb/ccc/commons/annotation/impl/NotNullValidation.java
@@ -12,6 +12,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * <a>Title: NotNullValidation </a>
@@ -28,23 +31,29 @@ public class NotNullValidation {
 
     private static final Logger log = LoggerFactory.getLogger(NotNullValidation.class);
 
-    @Pointcut(value = "execution(public * com.kapcb.ccc.controller.*.*(@com.kapcb.ccc.commons.annotation.NotNull (*), ..))")
+    @Pointcut(value = "execution(public * com.kapcb.ccc.controller.*.*(@com.kapcb.ccc.commons.annotation.NotNull (*), ..))||" +
+            "execution(public * com.kapcb.ccc.controller.*.*(*, @com.kapcb.ccc.commons.annotation.NotNull (*)))||" +
+            "execution(public * com.kapcb.ccc.controller.*.*(@com.kapcb.ccc.commons.annotation.NotNull (*), @com.kapcb.ccc.commons.annotation.NotNull (*)))")
     public void cut() {
     }
 
     @Around(value = "cut()")
     public Object process(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
         Signature signature = proceedingJoinPoint.getSignature();
+        String name = signature.getName();
+        log.info("the signature name is : " + name);
         MethodSignature methodSignature = (MethodSignature) signature;
         Method method = methodSignature.getMethod();
         log.info("the method is : " + method.toString());
-        String a = (String) proceedingJoinPoint.getArgs()[0];
-        log.info("the arg is : " + a);
-        boolean value = false;
-        String message = null;
-
-        if (StringUtils.isBlank(a) || StringUtils.equals(null, a)) {
-            return "the required params should not be null or empty or blank";
+        List<String> list = new ArrayList<>();
+        Object[] args = proceedingJoinPoint.getArgs();
+        String s = Arrays.toString(args);
+        log.info("the args array is : " + s);
+        for (Object arg : proceedingJoinPoint.getArgs()) {
+            String a = (String) arg;
+            if (StringUtils.isBlank(a) || StringUtils.equals(null, a)) {
+                return "the required params should not be null or empty or blank";
+            }
         }
         log.info("kapcb nb");
         return proceedingJoinPoint.proceed();
