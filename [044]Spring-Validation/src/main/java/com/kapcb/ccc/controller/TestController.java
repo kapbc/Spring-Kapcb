a0ad9f505032b897df886565dc3565c77af9f65e
kapcb <55942617+kapbc@users.noreply.github.com>
2021-01-26 09:25:42 +0800

    Update TestController.java

--- [044]Spring-Validation/src/main/java/com/kapcb/ccc/controller/TestController.java
+++ [044]Spring-Validation/src/main/java/com/kapcb/ccc/controller/TestController.java
@@ -4,7 +4,6 @@ import com.kapcb.ccc.commons.annotation.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
