0b431cae310949199fae4fd3d85a093b939b915c
Eircc <2530628867@qq.com>
2021-01-30 12:35:29 +0800

    Spring-IOC-Principle

---
+++ [001]Spring-IOC-Principle/src/main/java/com/kapcb/ccc/factory/PromotionInstanceFactory.java
@@ -0,0 +1,31 @@
+package com.kapcb.ccc.factory;
+
+import com.kapcb.ccc.service.IUserService;
+
+/**
+ * <a>Title: PromotionInstanceFactory </a>
+ * <a>Author: kapcb <a>
+ * <a>Description: <a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2021/1/30 12:24
+ */
+public class PromotionInstanceFactory {
+
+    private PromotionInstanceFactory() {
+    }
+
+    public static IUserService getUserService(Class<? extends IUserService> clazz) {
+        if (clazz != null) {
+            try {
+                IUserService iUserService = clazz.newInstance();
+                return iUserService;
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+        return null;
+    }
+}
