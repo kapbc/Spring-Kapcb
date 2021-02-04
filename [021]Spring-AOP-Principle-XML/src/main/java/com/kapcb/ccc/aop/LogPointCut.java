adefd4470998f179f785eb67774c11fcf953e09d
Eircc <2530628867@qq.com>
2021-02-04 22:05:47 +0800

    Spring-AOP-Principle-XML

--- [021]Spring-AOP-Principle-XML/src/main/java/com/kapcb/ccc/aop/LogPointCut.java
+++ [021]Spring-AOP-Principle-XML/src/main/java/com/kapcb/ccc/aop/LogPointCut.java
@@ -1,5 +1,10 @@
 package com.kapcb.ccc.aop;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.Signature;
+
+import java.util.Arrays;
+
 /**
  * <a>Title: LogPointCut </a>
  * <a>Author: kapcb <a>
@@ -10,4 +15,26 @@ package com.kapcb.ccc.aop;
  * @date 2021/2/4 21:53
  */
 public class LogPointCut {
+
+    public Object pointCut(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        Object result = null;
+        Object[] args = proceedingJoinPoint.getArgs();
+        Signature signature = proceedingJoinPoint.getSignature();
+        String name = signature.getName();
+        try {
+            // @Before()
+            System.out.println("the before advance method : " + name + " is begin");
+            result = proceedingJoinPoint.proceed(args);
+            // @AfterReturning()
+            System.out.println("the afterReturning advance method : " + name + " is begin, the arguments is : " + Arrays.toString(args));
+        } catch (Exception e) {
+            // @AfterThrowing()
+            System.out.println("the afterThrowing advance method : " + name + " is begin, the arguments is : " + Arrays.toString(args));
+            throw new RuntimeException(e.getMessage());
+        } finally {
+            System.out.println("the after advance method : " + name + " is begin, the return result is : " + result);
+        }
+
+        return result;
+    }
 }
