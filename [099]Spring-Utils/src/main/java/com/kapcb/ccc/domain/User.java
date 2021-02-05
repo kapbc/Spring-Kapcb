ef3e014b122a29e94a992344264764a929990c4d
kapcb <55942617+kapbc@users.noreply.github.com>
2021-02-05 12:46:32 +0800

    Create User.java

---
+++ [099]Spring-Utils/src/main/java/com/kapcb/ccc/domain/User.java
@@ -0,0 +1,29 @@
+package com.kapcb.ccc.commons.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+/**
+ * <a>Title: User </a>
+ * <a>Author: kapcb <a>
+ * <a>Description：<a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2021/2/3-9:16
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@SuppressWarnings("serial")
+public class User implements Serializable {
+
+    private Long userId;
+    private String username;
+    private String password;
+    private LocalDateTime birthday;
+}
