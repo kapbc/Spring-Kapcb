b4203f722627528be33d8daeceab3000b5b57280
Eircc <2530628867@qq.com>
2021-02-05 21:59:39 +0800

    Spring-AOP-JDK

---
+++ [023]Spring-AOP-JDK/src/main/java/com/kapcb/ccc/domain/User.java
@@ -0,0 +1,48 @@
+package com.kapcb.ccc.domain;
+
+/**
+ * <a>Title: User </a>
+ * <a>Author: kapcb <a>
+ * <a>Description: <a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2021/2/5 21:58
+ */
+public class User {
+
+    private String username;
+    private String password;
+
+    public User() {
+    }
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
---
+++ [023]Spring-AOP-JDK/src/main/java/com/kapcb/ccc/service/IUserService.java
@@ -0,0 +1,16 @@
+package com.kapcb.ccc.service;
+
+
+/**
+ * <a>Title: IUserService </a>
+ * <a>Author: kapcb <a>
+ * <a>Description: <a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2021/2/5 21:54
+ */
+public interface IUserService {
+
+
+}
