43b76d084d6b486659aa4fe97f5a76e86e96144f
kapcb <55942617+kapbc@users.noreply.github.com>
2021-02-07 12:17:00 +0800

    Update SystemController.java

--- [099]Spring-Utils/src/main/java/com/kapcb/ccc/controller/SystemController.java
+++ [099]Spring-Utils/src/main/java/com/kapcb/ccc/controller/SystemController.java
@@ -2,6 +2,7 @@ package com.kapcb.ccc.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -19,9 +20,28 @@ public class SystemController {
 
     private static final Logger log = LoggerFactory.getLogger(SystemController.class);
 
+    @Value(value = "#{propertiesReader['kapcb.properties.awesome']}")
+    private String kapcbNb;
+
+    @Value(value = "#{propertiesReader['system.properties.author']}")
+    private String kapcb;
+
+//    @Value(value = "${kapcb.properties.awesome}")
+//    private String nb;
+//
+//    @Value(value = "${system.properties.author}")
+//    private String awesome;
+
+
     @GetMapping("/")
     public String index() {
         log.warn("come into index page");
+        System.out.println("===============test properties================");
+        System.out.println("kapcbNb = " + kapcbNb);
+        System.out.println("kapcb = " + kapcb);
+//        System.out.println("nb = " + nb);
+//        System.out.println("awesome = " + awesome);
+        System.out.println("===============test properties================");
         return "index";
     }
 }
