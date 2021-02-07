10c86c1ab7610ff53811fa6dc38ef0d44a585e1c
kapcb <55942617+kapbc@users.noreply.github.com>
2021-02-07 10:42:04 +0800

    Update ContextConfiguration.java

--- [099]Spring-Utils/src/main/java/com/kapcb/ccc/config/ContextConfiguration.java
+++ [099]Spring-Utils/src/main/java/com/kapcb/ccc/config/ContextConfiguration.java
@@ -1,11 +1,6 @@
 package com.kapcb.ccc.commons.config;
 
-import org.springframework.beans.factory.config.PropertiesFactoryBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.core.io.Resource;
 
 
 /**
@@ -18,17 +13,6 @@ import org.springframework.core.io.Resource;
  * @date 2021/1/29-9:34
  */
 @Configuration
-@ComponentScan(basePackages = {"com.kapcb.ccc"})
 public class ContextConfiguration {
 
-
-    @Bean
-    public PropertiesFactoryBean propertiesFactoryBean() {
-        PropertiesFactoryBean propertiesFactoryBean = new PropertiesFactoryBean();
-        Resource resourceKapcb = new FileSystemResource("classpath:properties/kapcb.properties");
-        Resource resourceSystem = new FileSystemResource("classpath:properties/system.properties");
-        Resource[] location = new Resource[]{resourceKapcb, resourceSystem};
-        propertiesFactoryBean.setLocations(location);
-        return propertiesFactoryBean;
-    }
 }
