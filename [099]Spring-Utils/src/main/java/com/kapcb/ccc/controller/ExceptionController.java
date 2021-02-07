dd4695683469676b69c83f544e641a8f41e85b3e
kapcb <55942617+kapbc@users.noreply.github.com>
2021-02-07 10:43:00 +0800

    Create ExceptionController.java

---
+++ [099]Spring-Utils/src/main/java/com/kapcb/ccc/controller/ExceptionController.java
@@ -0,0 +1,33 @@
+package com.kapcb.ccc.controller;
+
+import com.kapcb.ccc.commons.utils.JsonUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+/**
+ * <a>Title: ExceptionController </a>
+ * <a>Author: kapcb <a>
+ * <a>Description：<a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2021/2/7-9:35
+ */
+@ControllerAdvice
+public class ExceptionController {
+
+    private static final Logger log = LoggerFactory.getLogger(ExceptionController.class);
+
+    @ExceptionHandler(Exception.class)
+    public String handlerSystemException(Exception e) {
+        log.error("handler system exception, the exception message is : " + e.getMessage());
+        JsonUtil.JsonBuilder jsonBuilder = new JsonUtil.JsonBuilder();
+        return jsonBuilder
+                .put("message", e.getMessage())
+                .put("code", "401")
+                .put("data", "")
+                .builder();
+    }
+}
