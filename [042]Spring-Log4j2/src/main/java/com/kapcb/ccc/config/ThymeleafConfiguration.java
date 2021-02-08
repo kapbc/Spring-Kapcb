018cdaeea20a4c25fe110f437cc8dbe844420672
kapcb <55942617+kapbc@users.noreply.github.com>
2021-02-08 10:07:04 +0800

    Create ThymeleafConfiguration.java

---
+++ [042]Spring-Log4j2/src/main/java/com/kapcb/ccc/config/ThymeleafConfiguration.java
@@ -0,0 +1,48 @@
+package com.kapcb.ccc.commons.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+
+import java.nio.charset.StandardCharsets;
+
+/**
+ * <a>Title: ThymeleafConfiguration </a>
+ * <a>Author: kapcb <a>
+ * <a>Description：<a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2021/2/7-16:39
+ */
+@Component
+public class ThymeleafConfiguration {
+
+    @Bean
+    public SpringResourceTemplateResolver springResourceTemplateResolver() {
+        SpringResourceTemplateResolver springResourceTemplateResolver = new SpringResourceTemplateResolver();
+        springResourceTemplateResolver.setSuffix(".html");
+        springResourceTemplateResolver.setPrefix("/WEB-INF/template/");
+        springResourceTemplateResolver.setTemplateMode("HTML");
+        springResourceTemplateResolver.setCharacterEncoding(StandardCharsets.UTF_8.toString());
+        springResourceTemplateResolver.setCacheable(false);
+        return springResourceTemplateResolver;
+    }
+
+    @Bean
+    public SpringTemplateEngine springTemplateEngine() {
+        SpringTemplateEngine springTemplateEngine = new SpringTemplateEngine();
+        springTemplateEngine.setTemplateResolver(springResourceTemplateResolver());
+        return springTemplateEngine;
+    }
+
+    @Bean
+    public ThymeleafViewResolver thymeleafViewResolver() {
+        ThymeleafViewResolver thymeleafViewResolver = new ThymeleafViewResolver();
+        thymeleafViewResolver.setCharacterEncoding(StandardCharsets.UTF_8.toString());
+        thymeleafViewResolver.setTemplateEngine(springTemplateEngine());
+        return thymeleafViewResolver;
+    }
+}
