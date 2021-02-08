1acb31dc1f4f5fd6cb432f9749246fb4cb3d0b7e
kapcb <55942617+kapbc@users.noreply.github.com>
2021-02-08 10:35:55 +0800

    Update SystemController.java

--- [042]Spring-Log4j2/src/main/java/com/kapcb/ccc/controller/SystemController.java
+++ [042]Spring-Log4j2/src/main/java/com/kapcb/ccc/controller/SystemController.java
@@ -1,6 +1,12 @@
 package com.kapcb.ccc.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * <a>Title: SystemController </a>
@@ -12,5 +18,23 @@ import org.springframework.stereotype.Controller;
  * @date 2021/2/7-16:41
  */
 @Controller
+@RequestMapping("/")
 public class SystemController {
+
+    private static final Logger log = LoggerFactory.getLogger(SystemController.class);
+
+    @Value(value = "#{propertiesReader['kapcb.awesome.man']}")
+    private String kapcb;
+
+    @Value(value = "#{propertiesReader['system.kapcb.nb']}")
+    private String system;
+
+    @GetMapping
+    @ResponseBody
+    public String execute() {
+        log.info("come into the index page");
+        log.info("the kapcb from kapcb properties is : {} ", kapcb);
+        log.info("the system from system properties is : {}", system);
+        return "index";
+    }
 }
