c3a493a9080c32787f0136233dafda115fb4df18
kapcb <55942617+kapbc@users.noreply.github.com>
2021-02-08 10:08:46 +0800

    Create IUserService.java

---
+++ [042]Spring-Log4j2/src/main/java/com/kapcb/ccc/service/IUserService.java
@@ -0,0 +1,13 @@
+package com.kapcb.ccc.service;
+
+/**
+ * <a>Title: UserService </a>
+ * <a>Author: kapcb <a>
+ * <a>Description：<a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2021/2/7-16:41
+ */
+public interface IUserService {
+}
