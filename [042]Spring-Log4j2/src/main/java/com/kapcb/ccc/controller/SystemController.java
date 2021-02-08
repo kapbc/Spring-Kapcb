16c4cda38c447eabf76da1ba8e1b52f4e8b58681
kapcb <55942617+kapbc@users.noreply.github.com>
2021-02-08 12:03:38 +0800

    Update SystemController.java

--- [042]Spring-Log4j2/src/main/java/com/kapcb/ccc/controller/SystemController.java
+++ [042]Spring-Log4j2/src/main/java/com/kapcb/ccc/controller/SystemController.java
@@ -4,8 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
@@ -18,7 +18,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * @date 2021/2/7-16:41
  */
 @Controller
-@RequestMapping("/")
 public class SystemController {
 
     private static final Logger log = LoggerFactory.getLogger(SystemController.class);
@@ -35,9 +34,8 @@ public class SystemController {
     @Value("${system.kapcb.nb}")
     private String system;
 
-    @ResponseBody
-    @GetMapping(produces = "application/json;charset=utf-8")
-    public String execute() {
+    @GetMapping(value = "/", produces = "application/json;charset=utf-8")
+    public String execute(Model model) {
         log.info("come into the index page");
         log.info("====================={}========================", "@Value(\"#{propertiesReader['xxx.xxx.xxx']})\")");
         log.info("the kapcb from kapcb properties is : {} ", kapcb1);
@@ -45,11 +43,13 @@ public class SystemController {
         log.info("====================={}========================", "@Value(\"${xxx.xxx.xxx}\")");
         log.info("the kapcb from kapcb properties is : {} ", kapcb);
         log.info("the system from system properties is : {}", system);
+        model.addAttribute("kapcb", kapcb);
+        model.addAttribute("system", system);
         return "index";
     }
 
     @ResponseBody
-    @GetMapping(value = "exception", produces = "application/json;charset=utf-8")
+    @GetMapping(value = "/exception", produces = "application/json;charset=utf-8")
     public String systemHandler() {
         log.info("test the controller overall situation advance exception handler");
         throw new RuntimeException("your error, please check yourself! Thanks!");
