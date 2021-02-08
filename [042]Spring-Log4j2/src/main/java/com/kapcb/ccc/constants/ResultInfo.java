667f9b98ccea70697b282c1be5e2003dff94339c
kapcb <55942617+kapbc@users.noreply.github.com>
2021-02-08 10:42:08 +0800

    Update ResultInfo.java

--- [042]Spring-Log4j2/src/main/java/com/kapcb/ccc/constants/ResultInfo.java
+++ [042]Spring-Log4j2/src/main/java/com/kapcb/ccc/constants/ResultInfo.java
@@ -16,9 +16,9 @@ public enum ResultInfo {
      */
     SUCCESS("200", "process request success", "success"),
     FAIL("201", "process request fail", "fail"),
-    JSON_RESULT_CODE("code", "json result api code"),
+    JSON_RESULT_CODE("code", "401"),
     JSON_RESULT_MESSAGE("message", "system error"),
-    JSON_RESULT_DATA("data", "401"),
+    JSON_RESULT_DATA("data", "json result api empty result data key"),
     JSON_RESULT_NULL_DATA("", "json result api empty result data"),
     ;
     private final String message;
