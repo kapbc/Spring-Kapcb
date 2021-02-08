485cd395fd4ea80b0db67bd79695466c8f2b904d
kapcb <55942617+kapbc@users.noreply.github.com>
2021-02-08 10:09:02 +0800

    Create IUserServiceImpl.java

---
+++ [042]Spring-Log4j2/src/main/java/com/kapcb/ccc/service/impl/IUserServiceImpl.java
@@ -0,0 +1,17 @@
+package com.kapcb.ccc.service.impl;
+
+import com.kapcb.ccc.service.IUserService;
+import org.springframework.stereotype.Service;
+
+/**
+ * <a>Title: IUserServiceImpl </a>
+ * <a>Author: kapcb <a>
+ * <a>Description：<a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2021/2/7-16:41
+ */
+@Service
+public class IUserServiceImpl implements IUserService {
+}
