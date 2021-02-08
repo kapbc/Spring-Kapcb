cfb6660527ef8fdc5a2d996e9703ba647a16cbe3
kapcb <55942617+kapbc@users.noreply.github.com>
2021-02-08 10:08:28 +0800

    Create SystemController.java

---
+++ [042]Spring-Log4j2/src/main/java/com/kapcb/ccc/controller/SystemController.java
@@ -0,0 +1,16 @@
+package com.kapcb.ccc.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * <a>Title: SystemController </a>
+ * <a>Author: kapcb <a>
+ * <a>Description：<a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2021/2/7-16:41
+ */
+@Controller
+public class SystemController {
+}
