b194d0fac6f6a9e961a3a65930ff8a1f72764e28
kapcb <55942617+kapbc@users.noreply.github.com>
2021-02-08 10:07:34 +0800

    Create ResultInfo.java

---
+++ [042]Spring-Log4j2/src/main/java/com/kapcb/ccc/constants/ResultInfo.java
@@ -0,0 +1,13 @@
+package com.kapcb.ccc.commons.constants;
+
+/**
+ * <a>Title: ResultInfo </a>
+ * <a>Author: kapcb <a>
+ * <a>Description：<a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2021/2/8-9:39
+ */
+public enum  ResultInfo {
+}
