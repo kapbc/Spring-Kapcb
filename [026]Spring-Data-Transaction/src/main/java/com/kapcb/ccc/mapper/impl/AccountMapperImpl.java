005afbe5219f755124a5b08995510db5681738dc
Eircc <2530628867@qq.com>
2021-02-10 11:38:28 +0800

    Spring-Data-Transaction

--- [026]Spring-Data-Transaction/src/main/java/com/kapcb/ccc/mapper/impl/AccountMapperImpl.java
+++ [026]Spring-Data-Transaction/src/main/java/com/kapcb/ccc/mapper/impl/AccountMapperImpl.java
@@ -5,9 +5,13 @@ import com.kapcb.ccc.mapper.AccountMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 
 /**
  * <a>Title: AccountMapperImpl </a>
@@ -21,10 +25,10 @@ import java.math.BigDecimal;
 @Repository(value = "accountMapper")
 public class AccountMapperImpl implements AccountMapper {
 
-    private static final String BD_NAME = "tb_account";
     private static final String DB_ID = "id";
-    private static final String DB_USERNAME = "username";
+    private static final String BD_NAME = "tb_account";
     private static final String DB_BALANCE = "balance";
+    private static final String DB_USERNAME = "username";
 
     @Autowired
     @Qualifier(value = "jdbcTemplate")
@@ -33,7 +37,19 @@ public class AccountMapperImpl implements AccountMapper {
 
     @Override
     public BigDecimal getAccountBalance(String userId, String username) {
-        return null;
+        StringBuilder sqlString = new StringBuilder();
+        sqlString.append("SELECT ").append(DB_BALANCE);
+        sqlString.append(" FROM ").append(DB_USERNAME);
+        sqlString.append(" WHERE ").append(DB_USERNAME).append(" = ");
+        sqlString.append(username).append(" AND ").append(DB_ID).append(" = ").append(userId);
+        System.out.println("the sql String is : " + sqlString.toString());
+        Object result = jdbcTemplate.queryForObject(sqlString.toString(), new RowMapper() {
+            @Override
+            public Object mapRow(ResultSet resultSet, int i) throws SQLException {
+                return null;
+            }
+        });
+        return ;
     }
 
     @Override
