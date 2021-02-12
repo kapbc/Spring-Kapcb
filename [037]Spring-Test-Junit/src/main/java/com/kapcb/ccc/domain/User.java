a6a84447af5c12182c4c0c0c89090f6fc0a22af7
Eircc <2530628867@qq.com>
2021-02-12 23:22:12 +0800

    Spring-Test-Junit

---
+++ [037]Spring-Test-Junit/src/main/java/com/kapcb/ccc/domain/User.java
@@ -0,0 +1,13 @@
+package com.kapcb.ccc.domain;
+
+/**
+ * <a>Title: User </a>
+ * <a>Author: kapcb <a>
+ * <a>Description: <a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2021/2/12 23:21
+ */
+public class User {
+}
