26cbb81f046b8ec5a0404063665716e163694abc
Eircc <2530628867@qq.com>
2021-02-12 13:29:58 +0800

    Spring-Log-Log4j2

---
+++ [031]Spring-Log-Log4j2/src/main/java/com/kapcb/ccc/service/ILogService.java
@@ -0,0 +1,13 @@
+package com.kapcb.ccc.service;
+
+/**
+ * <a>Title: ILogService </a>
+ * <a>Author: kapcb <a>
+ * <a>Description: <a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2021/2/12 13:22
+ */
+public interface ILogService {
+}
---
+++ [031]Spring-Log-Log4j2/src/main/java/com/kapcb/ccc/service/impl/ILogServiceImpl.java
@@ -0,0 +1,22 @@
+package com.kapcb.ccc.service.impl;
+
+import com.kapcb.ccc.service.ILogService;
+import org.apache.commons.logging.LogFactory;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Component;
+
+/**
+ * <a>Title: LogServiceImpl </a>
+ * <a>Author: kapcb <a>
+ * <a>Description: <a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2021/2/12 13:21
+ */
+@Component(value = "logService")
+public class ILogServiceImpl implements ILogService {
+
+    Logger logger =
+
+}
