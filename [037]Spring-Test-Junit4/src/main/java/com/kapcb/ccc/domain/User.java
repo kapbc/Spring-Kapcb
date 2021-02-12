cfd9d4d311e8f66710b425d59968c44f5b87bb74
Eircc <2530628867@qq.com>
2021-02-12 23:51:20 +0800

    Spring-Test-Junit4

--- [037]Spring-Test-Junit4/src/main/java/com/kapcb/ccc/domain/User.java
+++ [037]Spring-Test-Junit4/src/main/java/com/kapcb/ccc/domain/User.java
@@ -10,4 +10,50 @@ package com.kapcb.ccc.domain;
  * @date 2021/2/12 23:31
  */
 public class User {
+
+    private int age;
+    private String email;
+    private String username;
+
+    public User() {
+    }
+
+    public User(int age, String email, String username) {
+        this.age = age;
+        this.email = email;
+        this.username = username;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "age=" + age +
+                ", email='" + email + '\'' +
+                ", username='" + username + '\'' +
+                '}';
+    }
 }
--- [037]Spring-Test-Junit4/src/test/java/TestDrivenDevelopment.java
+++ [037]Spring-Test-Junit4/src/test/java/TestDrivenDevelopment.java
@@ -1,3 +1,9 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
 /**
  * <a>Title: TestDrivenDevelopment </a>
  * <a>Author: kapcb <a>
@@ -7,6 +13,18 @@
  * @version 1.0.0
  * @date 2021/2/12 23:28
  */
-
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:spring/spring.xml"})
 public class TestDrivenDevelopment {
+
+    /**
+     *
+     * RunWith(SpringJUnit4ClassRunner.class)
+     * 用于指定junit运行环境，是junit提供给其他框架测试环境接口扩展，为了便于使用spring的依赖注入，
+     * spring提供了org.springframework.test.context.junit4.SpringJUnit4ClassRunner作为Junit测试环境
+     */
+    @Test
+    public void testSpringJunit4ClassRunner(){
+
+    }
 }
