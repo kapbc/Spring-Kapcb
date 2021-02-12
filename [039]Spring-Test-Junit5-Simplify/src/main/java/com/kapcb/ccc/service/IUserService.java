223f04243f7e51dc404084f3ba761e87a6a56b2c
Eircc <2530628867@qq.com>
2021-02-13 00:27:27 +0800

    Spring-Test-Junit5-Simplify

---
+++ [039]Spring-Test-Junit5-Simplify/src/main/java/com/kapcb/ccc/service/IUserService.java
@@ -0,0 +1,13 @@
+package com.kapcb.ccc.service;
+
+/**
+ * <a>Title: IUserService </a>
+ * <a>Author: kapcb <a>
+ * <a>Description: <a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2021/2/13 0:24
+ */
+public interface IUserService {
+}
---
+++ [039]Spring-Test-Junit5-Simplify/src/main/java/com/kapcb/ccc/service/impl/IUserServiceImpl.java
@@ -0,0 +1,15 @@
+package com.kapcb.ccc.service.impl;
+
+import com.kapcb.ccc.service.IUserService;
+
+/**
+ * <a>Title: IUserServiceImpl </a>
+ * <a>Author: kapcb <a>
+ * <a>Description: <a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2021/2/13 0:24
+ */
+public class IUserServiceImpl implements IUserService {
+}
