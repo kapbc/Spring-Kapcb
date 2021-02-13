ce9aaecc22ee1236735f3c6eb7167e90a28bd474
Eircc <2530628867@qq.com>
2021-02-13 20:43:00 +0800

    Spring-Annotation-Value

---
+++ [042]Spring-Annotaion-Value2/src/main/java/com/kapcb/ccc/commons/config/SpringContextConfiguration.java
@@ -0,0 +1,22 @@
+package com.kapcb.ccc.commons.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * <a>Title: ContextConfiguration </a>
+ * <a>Author: kapcb <a>
+ * <a>Description: <a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2021/2/13 20:40
+ */
+@Configuration
+public class SpringContextConfiguration {
+
+    private static final Logger log = LoggerFactory.getLogger(SpringContextConfiguration.class);
+
+
+}
---
+++ [042]Spring-Annotaion-Value2/src/main/java/com/kapcb/ccc/domain/SystemProperties.java
@@ -0,0 +1,13 @@
+package com.kapcb.ccc.domain;
+
+/**
+ * <a>Title: SystemProperties </a>
+ * <a>Author: kapcb <a>
+ * <a>Description: <a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2021/2/13 20:39
+ */
+public class SystemProperties {
+}
