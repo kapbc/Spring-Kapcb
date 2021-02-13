51543ce924c1a6ec049471a45d0f8eb79d1115db
Eircc <2530628867@qq.com>
2021-02-13 20:35:11 +0800

    Spring-Annotation-Value

--- [041]Spring-Annotaion-Value/src/test/java/TestDrivenDevelopment.java
+++ [041]Spring-Annotaion-Value/src/test/java/TestDrivenDevelopment.java
@@ -24,12 +24,25 @@ public class TestDrivenDevelopment {
     @Value(value = "${spring.annotation.value.test}")
     private String kapcbProperties;
 
+    @Value(value = "#{propertiesFactoryBean['spring.annotation.configuration.username']}")
+    private String kapcbPropertiesUsername;
+
+    @Value(value = "#{propertiesFactoryBean['system.properties.kapcb.must.nb']}")
+    private String systemPropertiesNb;
 
     @Test
     public void testAnnotationValue() {
         log.warn("begin to get properties from ioc context....");
         System.out.println("systemProperties = " + systemProperties);
         System.out.println("kapcbProperties = " + kapcbProperties);
+        log.warn("process get properties from ioc context success...");
+    }
+
+    @Test
+    public void testAnnotationValuePropertiesFactoryBean() {
+        log.warn("begin to get properties from ioc context...");
+        System.out.println("kapcbPropertiesUsername = " + kapcbPropertiesUsername);
+        System.out.println("systemPropertiesNb = " + systemPropertiesNb);
         log.warn("process get properties from ioc context success...");
     }
 }
