051b408d6abaee545902c08c33225c56bd7590b8
Eircc <2530628867@qq.com>
2021-02-13 20:22:48 +0800

    Spring-Annotation-Value

--- [041]Spring-Annotaion-Value/src/test/java/TestDrivenDevelopment.java
+++ [041]Spring-Annotaion-Value/src/test/java/TestDrivenDevelopment.java
@@ -1,3 +1,7 @@
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 /**
@@ -9,6 +13,23 @@ import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
  * @version 1.0.0
  * @date 2021/2/13 10:31
  */
-@SpringJUnitConfig(locations = {""})
+@SpringJUnitConfig(locations = {"classpath:spring/spring.xml"})
 public class TestDrivenDevelopment {
+
+    private static final Logger log = LoggerFactory.getLogger(TestDrivenDevelopment.class);
+
+    @Value("${system.properties.kapcb.nb}")
+    private String systemProperties;
+
+    @Value(value = "${spring.annotation.value.test}")
+    private String kapcbProperties;
+
+
+    @Test
+    public void testAnnotationValue() {
+        log.warn("begin to get properties from ioc context....");
+        System.out.println("systemProperties = " + systemProperties);
+        System.out.println("kapcbProperties = " + kapcbProperties);
+        log.warn("process get properties from ioc context success...");
+    }
 }
