f91a0fe2a02f33da5feea1dd6dd944baa9f02ef3
Eircc <2530628867@qq.com>
2021-02-13 10:32:05 +0800

    Spring-Annotation-Value

---
+++ [041]Spring-Annotaion-Value/src/test/java/TestDrivenDevelopment.java
@@ -0,0 +1,14 @@
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+/**
+ * <a>Title: TestDrivenDevelopment </a>
+ * <a>Author: kapcb <a>
+ * <a>Description: <a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2021/2/13 10:31
+ */
+@SpringJUnitConfig(locations = {""})
+public class TestDrivenDevelopment {
+}
