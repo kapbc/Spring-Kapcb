2f8e4bcfb5991af06c0214403f7ccef16d13feea
Eircc <2530628867@qq.com>
2021-02-14 15:01:46 +0800

    Spring-MVC-Params

---
+++ [049]Spring-MVC-Params/src/main/java/com/kapcb/ccc/controller/ExceptionController.java
@@ -0,0 +1,14 @@
+package com.kapcb.ccc.controller;
+
+/**
+ * <a>Title: ExceptionController </a>
+ * <a>Author: kapcb <a>
+ * <a>Description: <a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2021/2/14 14:58
+ */
+
+public class ExceptionController {
+}
---
+++ [049]Spring-MVC-Params/src/main/java/com/kapcb/ccc/controller/SystemController.java
@@ -0,0 +1,13 @@
+package com.kapcb.ccc.controller;
+
+/**
+ * <a>Title: SystemController </a>
+ * <a>Author: kapcb <a>
+ * <a>Description: <a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2021/2/14 14:58
+ */
+public class SystemController {
+}
