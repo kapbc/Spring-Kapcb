5bc71b60b180816da3440baef688df4076b2a630
Eircc <2530628867@qq.com>
2021-02-14 13:45:46 +0800

    Spring-MVC-HelloWorld

---
+++ [045]Spring-MVC-HelloWorld/src/main/java/com/kapcb/ccc/controller/SystemController.java
@@ -0,0 +1,36 @@
+package com.kapcb.ccc.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * <a>Title: SystemController </a>
+ * <a>Author: kapcb <a>
+ * <a>Description: <a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2021/2/14 13:37
+ */
+@Controller
+@RequestMapping(path = "/")
+public class SystemController {
+
+    private static final Logger log = LoggerFactory.getLogger(SystemController.class);
+
+    @GetMapping(produces = "application/json; charset=UTF-8")
+    public String index(Model model) {
+        log.warn("come into index page....");
+        model.addAttribute("kapcb","kapcb");
+        return "index";
+    }
+
+    @GetMapping(produces = "application/json; charset=UTF-8")
+    public String helloSpringMVC() {
+
+    }
+}
