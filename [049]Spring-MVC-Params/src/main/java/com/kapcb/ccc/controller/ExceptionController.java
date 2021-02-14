e85b37966fc658b000688f425e0af7cd39acb03a
Eircc <2530628867@qq.com>
2021-02-14 15:13:49 +0800

    Spring-MVC-Params

--- [049]Spring-MVC-Params/src/main/java/com/kapcb/ccc/controller/ExceptionController.java
+++ [049]Spring-MVC-Params/src/main/java/com/kapcb/ccc/controller/ExceptionController.java
@@ -1,5 +1,11 @@
 package com.kapcb.ccc.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 /**
  * <a>Title: ExceptionController </a>
  * <a>Author: kapcb <a>
@@ -9,6 +15,15 @@ package com.kapcb.ccc.controller;
  * @version 1.0.0
  * @date 2021/2/14 14:58
  */
-
+@ResponseBody
+@ControllerAdvice
 public class ExceptionController {
+
+    private static final Logger log = LoggerFactory.getLogger(ExceptionController.class);
+
+    @ExceptionHandler(value = {Exception.class})
+    public String handlerSystemException(Exception e) {
+        log.error("process system exception, the exception message is : " + e.getMessage());
+        return e.getMessage();
+    }
 }
--- [049]Spring-MVC-Params/src/main/java/com/kapcb/ccc/controller/SystemController.java
+++ [049]Spring-MVC-Params/src/main/java/com/kapcb/ccc/controller/SystemController.java
@@ -1,5 +1,8 @@
 package com.kapcb.ccc.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * <a>Title: SystemController </a>
  * <a>Author: kapcb <a>
@@ -9,5 +12,9 @@ package com.kapcb.ccc.controller;
  * @version 1.0.0
  * @date 2021/2/14 14:58
  */
+@Controller
+@RequestMapping(path = "/")
 public class SystemController {
+
+
 }
