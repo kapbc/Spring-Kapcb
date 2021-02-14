1e2701c6494b7f1f2bf39ead5cc4d29a0793138b
Eircc <2530628867@qq.com>
2021-02-14 14:49:08 +0800

    Spring-MVC-HelloWorld

---
+++ [047]Spring-MVC-HelloWorld3/src/main/java/com/kapcb/ccc/controller/SystemController.java
@@ -0,0 +1,18 @@
+package com.kapcb.ccc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * <a>Title: SystemController </a>
+ * <a>Author: kapcb <a>
+ * <a>Description: <a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2021/2/14 14:45
+ */
+@Controller
+@RequestMapping
+public class SystemController {
+}
