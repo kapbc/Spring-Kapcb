cf344cd79b67882e7397eeed104a81b83ff2ecbf
Eircc <2530628867@qq.com>
2021-02-17 14:58:52 +0800

    Spring-MVC-Params

---
+++ [049]Spring-MVC-Params/src/test/java/TestDrivenDevelopment.java
@@ -0,0 +1,14 @@
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+/**
+ * <a>Title: TestDrivenDevelopment </a>
+ * <a>Author: kapcb <a>
+ * <a>Description: <a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2021/2/17 14:57
+ */
+@SpringJUnitConfig(locations = {"classpath:spring/spring.xml", "classpath:spring/spring-mvc.xml"})
+public class TestDrivenDevelopment {
+}
