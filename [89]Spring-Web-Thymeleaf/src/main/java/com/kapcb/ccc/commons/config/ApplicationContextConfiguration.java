89578bb0e2e4490a2510b25dfce20ea22f80fa1b
kapcb <55942617+kapbc@users.noreply.github.com>
2021-02-20 15:25:55 +0800

    Create ApplicationContextConfiguration.java

---
+++ [89]Spring-Web-Thymeleaf/src/main/java/com/kapcb/ccc/commons/config/ApplicationContextConfiguration.java
@@ -0,0 +1,17 @@
+package com.kapcb.ccc.commons.config;
+
+/**
+ * <a>Title: ApplicationContextConfiguration </a>
+ * <a>Author: kapcb <a>
+ * <a>Description: <a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2021/2/19 21:47
+ */
+public class ApplicationContextConfiguration {
+
+    /**
+     * https://blog.csdn.net/Gyb_csdn/article/details/73771071?utm_medium=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-1.control&dist_request_id=7a254bcb-8119-4e39-91b2-c0873893e7f4&depth_1-utm_source=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-1.control
+     */
+}
