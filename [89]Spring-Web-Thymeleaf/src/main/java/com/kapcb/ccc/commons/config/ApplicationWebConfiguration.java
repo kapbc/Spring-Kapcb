b3dd2a702a5202759ce8e78ea3b0b0d5cae4400d
kapcb <55942617+kapbc@users.noreply.github.com>
2021-02-20 15:26:07 +0800

    Create ApplicationWebConfiguration.java

---
+++ [89]Spring-Web-Thymeleaf/src/main/java/com/kapcb/ccc/commons/config/ApplicationWebConfiguration.java
@@ -0,0 +1,71 @@
+package com.kapcb.ccc.commons.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+
+import java.nio.charset.StandardCharsets;
+
+/**
+ * <a>Title: ApplicationWebConfiguration </a>
+ * <a>Author: kapcb <a>
+ * <a>Description：<a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2021/2/20-15:15
+ */
+@EnableWebMvc       //启用Spring MVC组件
+@Configuration      //配置类
+@ComponentScan(basePackages = {"com.kapcb.ccc"})        //配置扫描的ctrl层
+public class ApplicationWebConfiguration implements WebMvcConfigurer {
+
+
+    /**
+     * Spring 5 的静态资源解析器
+     *
+     * @return SpringResourceTemplateResolver
+     */
+    @Bean
+    public SpringResourceTemplateResolver springResourceTemplateResolver() {
+        SpringResourceTemplateResolver springResourceTemplateResolver = new SpringResourceTemplateResolver();
+        springResourceTemplateResolver.setCacheable(false);
+        springResourceTemplateResolver.setTemplateMode("HTML");
+        springResourceTemplateResolver.setCharacterEncoding(StandardCharsets.UTF_8.toString());
+        springResourceTemplateResolver.setPrefix("/WEB-INF/template/");
+        springResourceTemplateResolver.setSuffix(".html");
+        return springResourceTemplateResolver;
+    }
+
+    /**
+     * Spring 5 的模板引擎
+     *
+     * @param springResourceTemplateResolver SpringResourceTemplateResolver
+     * @return SpringTemplateEngine
+     */
+    @Bean
+    public SpringTemplateEngine springTemplateEngine(SpringResourceTemplateResolver springResourceTemplateResolver) {
+        SpringTemplateEngine springTemplateEngine = new SpringTemplateEngine();
+        springTemplateEngine.setTemplateResolver(springResourceTemplateResolver);
+        return springTemplateEngine;
+    }
+
+    /**
+     * Thymeleaf 的试图解析
+     *
+     * @param springTemplateEngine springTemplateEngine
+     * @return ThymeleafViewResolver
+     */
+    @Bean
+    public ThymeleafViewResolver thymeleafViewResolver(SpringTemplateEngine springTemplateEngine) {
+        ThymeleafViewResolver thymeleafViewResolver = new ThymeleafViewResolver();
+        thymeleafViewResolver.setTemplateEngine(springTemplateEngine);
+        thymeleafViewResolver.setCharacterEncoding(StandardCharsets.UTF_8.toString());
+        return thymeleafViewResolver;
+    }
+}
