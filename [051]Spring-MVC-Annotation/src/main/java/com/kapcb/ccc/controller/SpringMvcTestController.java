fd1418a8692f974c274f92fcb08b458500b6d2e8
Eircc <2530628867@qq.com>
2021-02-21 22:45:39 +0800

    Spring-MVC-Annotation

---
+++ [051]Spring-MVC-Annotation/src/main/java/com/kapcb/ccc/controller/SpringMvcTestController.java
@@ -0,0 +1,13 @@
+package com.kapcb.ccc.controller;
+
+/**
+ * <a>Title: SpringMvcTestController </a>
+ * <a>Author: kapcb <a>
+ * <a>Description: <a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2021/2/21 22:41
+ */
+public class SpringMvcTestController {
+}
---
+++ [051]Spring-MVC-Annotation/src/main/java/com/kapcb/ccc/domain/User.java
@@ -0,0 +1,52 @@
+package com.kapcb.ccc.domain;
+
+import java.io.Serializable;
+
+/**
+ * <a>Title: User </a>
+ * <a>Author: kapcb <a>
+ * <a>Description: <a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2021/2/21 22:42
+ */
+public class User implements Serializable {
+
+    private static final long serialVersionUID = -3090405467171764979L;
+
+    private String username;
+    private String password;
+
+    public User() {
+    }
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
