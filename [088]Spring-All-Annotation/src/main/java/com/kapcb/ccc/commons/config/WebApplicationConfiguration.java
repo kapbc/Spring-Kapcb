cfa5c7b1e34309278a5db13d1a93ce7dc5dbfe17
Eircc <2530628867@qq.com>
2021-02-21 21:30:23 +0800

    Spring-All-Annotation

--- [088]Spring-All-Annotation/src/main/java/com/kapcb/ccc/commons/config/WebApplicationConfiguration.java
+++ [088]Spring-All-Annotation/src/main/java/com/kapcb/ccc/commons/config/WebApplicationConfiguration.java
@@ -2,6 +2,7 @@ package com.kapcb.ccc.commons.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -10,6 +11,9 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 
 /**
  * <a>Title: ApplicationWebConfiguration </a>
@@ -30,5 +34,34 @@ public class WebApplicationConfiguration implements WebMvcConfigurer {
 
     public WebApplicationConfiguration() {
         log.info("================begin to initial WebApplicationConfiguration......================");
+    }
+
+    @Bean
+    public SpringResourceTemplateResolver springResourceTemplateResolver() {
+        log.info("register SpringResourceTemplateResolver into spring application context...");
+        SpringResourceTemplateResolver springResourceTemplateResolver = new SpringResourceTemplateResolver();
+        springResourceTemplateResolver.setTemplateMode("HTML");
+        springResourceTemplateResolver.setPrefix("/WEB-INF/template/");
+        springResourceTemplateResolver.setSuffix(".html");
+        springResourceTemplateResolver.setCharacterEncoding("UTF-8");
+        springResourceTemplateResolver.setCacheable(false);
+        return springResourceTemplateResolver;
+    }
+
+    @Bean
+    public SpringTemplateEngine springTemplateEngine(SpringResourceTemplateResolver springResourceTemplateResolver) {
+        log.info("register SpringTemplateEngine into spring application context...");
+        SpringTemplateEngine springTemplateEngine = new SpringTemplateEngine();
+        springTemplateEngine.setTemplateResolver(springResourceTemplateResolver);
+        return springTemplateEngine;
+    }
+
+    @Bean
+    public ThymeleafViewResolver thymeleafViewResolver(SpringTemplateEngine springTemplateEngine) {
+        log.info("register ThymeleafViewResolver into spring application context...");
+        ThymeleafViewResolver thymeleafViewResolver = new ThymeleafViewResolver();
+        thymeleafViewResolver.setTemplateEngine(springTemplateEngine);
+        thymeleafViewResolver.setCharacterEncoding("UTF-8");
+        return thymeleafViewResolver;
     }
 }
