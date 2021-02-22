c0a55ab43de14a380a7403d3d568595280adaead
Eircc <2530628867@qq.com>
2021-02-22 22:23:43 +0800

    Spring-MVC-Annotation

--- [051]Spring-MVC-Annotation/src/main/java/com/kapcb/ccc/controller/SpringMvcTestController.java
+++ [051]Spring-MVC-Annotation/src/main/java/com/kapcb/ccc/controller/SpringMvcTestController.java
@@ -3,7 +3,9 @@ package com.kapcb.ccc.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * <a>Title: SpringMvcTestController </a>
@@ -15,9 +17,33 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @date 2021/2/21 22:41
  */
 @Controller
-@RequestMapping
+@RequestMapping(path = "SpringMVC")
 public class SpringMvcTestController {
 
     private static final Logger log = LoggerFactory.getLogger(SpringMvcTestController.class);
+
+    private static final String COMMON_VIEW_NAME = "success";
+
+    /**
+     * RequestParam 测试
+     *
+     * @param age      Integer
+     * @param username String
+     * @param password String
+     * @return String
+     */
+    @GetMapping(path = "userRequestParam")
+    public String userRequestParamTest(@RequestParam(name = "age", required = false) Integer age,
+                                       @RequestParam(value = "username") String username,
+                                       @RequestParam("password") String password) {
+        log.info("=================================");
+        log.info("come into user request param...");
+        log.info("the username is : {}", username);
+        log.info("the password is : " + password);
+        log.info("the age is : " + age);
+        log.info("=================================");
+        return COMMON_VIEW_NAME;
+    }
+
 
 }
