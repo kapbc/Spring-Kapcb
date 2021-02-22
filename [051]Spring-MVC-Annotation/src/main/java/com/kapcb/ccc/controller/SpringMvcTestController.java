9b1484c9c2619321bf848bba1931c5793a621bcc
Eircc <2530628867@qq.com>
2021-02-22 22:32:35 +0800

    Spring-MVC-Annotation

--- [051]Spring-MVC-Annotation/src/main/java/com/kapcb/ccc/controller/SpringMvcTestController.java
+++ [051]Spring-MVC-Annotation/src/main/java/com/kapcb/ccc/controller/SpringMvcTestController.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -58,6 +59,57 @@ public class SpringMvcTestController {
     public String userRequestBodyTest(@RequestBody String user) {
         log.info("come into user request body...");
         log.info("the request body user is : " + user);
+        return COMMON_VIEW_NAME;
+    }
+
+    /**
+     * PathVariable 测试
+     * 路径参数测试
+     *
+     * @param id String
+     * @return String
+     */
+    @PostMapping(value = "userPathVariable/{id}")
+    public String userPathVariableTest(@PathVariable(value = "id") String id) {
+        log.info("come into user path variable...");
+        log.info("the path variable is : " + id);
+        return COMMON_VIEW_NAME;
+    }
+
+    /**
+     * PathVariable 测试
+     * 路径参数测试
+     *
+     * @param id       String
+     * @param username String
+     * @return String
+     */
+    @PostMapping(value = "userPathVariable/{id}/{username}")
+    public String userPathVariableTest(@PathVariable("id") String id,
+                                       @PathVariable(name = "username") String username) {
+        log.info("come into user path variable...");
+        log.info("the path variable's id is : " + id);
+        log.info("the path variable's username is : " + username);
+        return COMMON_VIEW_NAME;
+    }
+
+    /**
+     * PathVariable 测试
+     * 路径参数测试
+     *
+     * @param id       String
+     * @param username String
+     * @param password String
+     * @return String
+     */
+    @PostMapping(value = "userPathVariable/{id}/{username}/{password}")
+    public String userPathVariableTest(@PathVariable("id") String id,
+                                       @PathVariable(name = "username") String username,
+                                       @PathVariable(value = "password") String password) {
+        log.info("come into user path variable...");
+        log.info("the path variable's id is : " + id);
+        log.info("the path variable's username is : " + username);
+        log.info("the path variable's password is : " + password);
         return COMMON_VIEW_NAME;
     }
 
