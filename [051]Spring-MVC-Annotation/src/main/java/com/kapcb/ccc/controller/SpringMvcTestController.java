178461f43db75b6258bffd6b80f9414234847db1
kapcb <55942617+kapbc@users.noreply.github.com>
2021-02-22 19:32:06 +0800

    Update SpringMvcTestController.java

--- [051]Spring-MVC-Annotation/src/main/java/com/kapcb/ccc/controller/SpringMvcTestController.java
+++ [051]Spring-MVC-Annotation/src/main/java/com/kapcb/ccc/controller/SpringMvcTestController.java
@@ -1,5 +1,10 @@
 package com.kapcb.ccc.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * <a>Title: SpringMvcTestController </a>
  * <a>Author: kapcb <a>
@@ -9,5 +14,10 @@ package com.kapcb.ccc.controller;
  * @version 1.0.0
  * @date 2021/2/21 22:41
  */
+@Controller
+@RequestMapping
 public class SpringMvcTestController {
+
+    private static final Logger log = LoggerFactory.getLogger(SpringMvcTestController.class);
+
 }
