5685a5a70360c3c62c50a4caa2025418efc865c3
Eircc <2530628867@qq.com>
2021-02-23 23:04:12 +0800

    Spring-AMQP-RabbitMQ

---
+++ [095]Spring-Empty-Demo2/src/test/java/TestDrivenDevelopment.java
@@ -0,0 +1,11 @@
+/**
+ * <a>Title: TestDrivenDevelopment </a>
+ * <a>Author: kapcb <a>
+ * <a>Description: <a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2021/2/23 23:03
+ */
+public class TestDrivenDevelopment {
+}
