6111f35bc92943c2c50b967e93ada30327c978bb
Eircc <2530628867@qq.com>
2021-02-23 22:55:32 +0800

    Spring-MVC-Annotation

--- [051]Spring-MVC-Annotation/src/main/java/com/kapcb/ccc/controller/SpringMvcTestController.java
+++ [051]Spring-MVC-Annotation/src/main/java/com/kapcb/ccc/controller/SpringMvcTestController.java
@@ -1,16 +1,23 @@
 package com.kapcb.ccc.controller;
 
+import com.kapcb.ccc.domain.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Map;
 
 /**
  * <a>Title: SpringMvcTestController </a>
