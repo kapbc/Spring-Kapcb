d095703e9d6717f16bfebc0608481a1893fcc44a
Eircc <2530628867@qq.com>
2021-02-23 23:18:12 +0800

    Spring-MVC-Exception

--- [052]Spring-MVC-Exception/src/main/java/com/kapcb/ccc/exception/SystemException.java
+++ [052]Spring-MVC-Exception/src/main/java/com/kapcb/ccc/exception/SystemException.java
@@ -1,5 +1,9 @@
 package com.kapcb.ccc.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 /**
  * <a>Title: SystemException </a>
  * <a>Author: kapcb <a>
@@ -9,5 +13,25 @@ package com.kapcb.ccc.exception;
  * @version 1.0.0
  * @date 2021/2/23 23:08
  */
-public class SystemException {
+public class SystemException extends Exception {
+
+    private static final Logger log = LoggerFactory.getLogger(SystemException.class);
+
+    private static final long serialVersionUID = 8415167801186847670L;
+
+    private String message;
+
+    public SystemException(String message) {
+        log.error("System error, the exception is : " + message);
+        this.message = message;
+    }
+
+    @Override
+    public String getMessage() {
+        return this.message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
--- [052]Spring-MVC-Exception/src/main/java/com/kapcb/ccc/exception/SystemExceptionResolver.java
+++ [052]Spring-MVC-Exception/src/main/java/com/kapcb/ccc/exception/SystemExceptionResolver.java
@@ -1,5 +1,13 @@
 package com.kapcb.ccc.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 /**
  * <a>Title: SystemExceptionResolver </a>
  * <a>Author: kapcb <a>
@@ -9,5 +17,21 @@ package com.kapcb.ccc.exception;
  * @version 1.0.0
  * @date 2021/2/23 23:08
  */
-public class SystemExceptionResolver {
+public class SystemExceptionResolver implements HandlerExceptionResolver {
+
+    private static final Logger log = LoggerFactory.getLogger(SystemExceptionResolver.class);
+
+    /**
+     * 转到具体的错误页面的方法
+     *
+     * @param httpServletRequest
+     * @param httpServletResponse
+     * @param o
+     * @param e
+     * @return
+     */
+    @Override
+    public ModelAndView resolveException(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) {
+        return null;
+    }
 }
