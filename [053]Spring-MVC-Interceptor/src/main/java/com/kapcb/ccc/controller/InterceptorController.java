14a39c7648645a4e0d5448b8fc6af6def944787c
Eircc <2530628867@qq.com>
2021-02-24 23:03:48 +0800

    Spring-MVC-Interceptor

--- [053]Spring-MVC-Interceptor/src/main/java/com/kapcb/ccc/controller/InterceptorController.java
+++ [053]Spring-MVC-Interceptor/src/main/java/com/kapcb/ccc/controller/InterceptorController.java
@@ -1,5 +1,11 @@
 package com.kapcb.ccc.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * <a>Title: InterceptorController </a>
  * <a>Author: kapcb <a>
@@ -9,5 +15,30 @@ package com.kapcb.ccc.controller;
  * @version 1.0.0
  * @date 2021/2/24 22:48
  */
+@Controller
+@RequestMapping(path = "test")
 public class InterceptorController {
+
+    private static final Logger log = LoggerFactory.getLogger(InterceptorController.class);
+
+    /**
+     * test with interceptor
+     *
+     * @return String
+     */
+    @GetMapping(path = "testWithInterceptor")
+    public String testInterceptor() {
+        log.info("come into test with interceptor...");
+        return "success";
+    }
+
+    /**
+     * test without interceptor
+     * @return
+     */
+    @GetMapping(value = "testWithoutInterceptor")
+    public String testWithoutInterceptor() {
+        log.info("come into test without interceptor...");
+        return "success";
+    }
 }
