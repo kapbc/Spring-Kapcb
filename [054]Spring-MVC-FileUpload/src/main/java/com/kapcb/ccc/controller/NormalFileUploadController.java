be5051d4e6b7aed8fd8cb9aa7f1f4169fadb884c
Eircc <2530628867@qq.com>
2021-02-25 22:28:50 +0800

    Spring-MVC-FileUpload

---
+++ [054]Spring-MVC-FileUpload/src/main/java/com/kapcb/ccc/controller/NormalFileUploadController.java
@@ -0,0 +1,13 @@
+package com.kapcb.ccc.controller;
+
+/**
+ * <a>Title: NormalFileUploadController </a>
+ * <a>Author: kapcb <a>
+ * <a>Description: <a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2021/2/25 22:24
+ */
+public class NormalFileUploadController {
+}
