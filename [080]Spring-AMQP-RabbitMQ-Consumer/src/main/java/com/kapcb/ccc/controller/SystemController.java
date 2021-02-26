85fb2a6c5b80a7cf832bec9f17c64c204586cd86
Eircc <2530628867@qq.com>
2021-02-26 22:20:54 +0800

    Spring-AMQP-RabbitMQ-Consumer

---
+++ [080]Spring-AMQP-RabbitMQ-Consumer/src/main/java/com/kapcb/ccc/controller/SystemController.java
@@ -0,0 +1,13 @@
+package com.kapcb.ccc.controller;
+
+/**
+ * <a>Title: SystemController </a>
+ * <a>Author: kapcb <a>
+ * <a>Description: <a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2021/2/26 22:16
+ */
+public class SystemController {
+}
---
+++ [080]Spring-AMQP-RabbitMQ-Consumer/src/main/java/com/kapcb/ccc/service/impl/IRabbitServiceImpl.java
@@ -0,0 +1,15 @@
+package com.kapcb.ccc.service.impl;
+
+import com.kapcb.ccc.service.IRabbitService;
+
+/**
+ * <a>Title: IRabbitServiceImpl </a>
+ * <a>Author: kapcb <a>
+ * <a>Description: <a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2021/2/26 22:16
+ */
+public class IRabbitServiceImpl implements IRabbitService {
+}
