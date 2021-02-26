5e750d9504c980da3ed53b231cdb92ebedab476a
kapcb <55942617+kapbc@users.noreply.github.com>
2021-02-26 12:46:50 +0800

    Create FanoutRabbitReceiverA.java

---
+++ [096]Spring-Empty-Demo1/src/main/java/com/kapcb/ccc/service/rabbit/fanout/FanoutRabbitReceiverA.java
@@ -0,0 +1,31 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+/**
+ * <a>Title: FanoutRabbitReceiver </a>
+ * <a>Author: kapcb <a>
+ * <a>Description：<a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2021/2/25-11:14
+ */
+@Component
+@RabbitListener(queues = "")
+public class FanoutRabbitReceiverA {
+
+    private static final Logger log = LoggerFactory.getLogger(FanoutRabbitReceiverA.class);
+
+    @RabbitHandler
+    public void processFanoutMessagesRabbitReceiveMessage(Map message) {
+        log.info("=======This is Fanout A Exchange======");
+        log.info("receive message from rabbit");
+        log.info("The message received from route key 'globalsource.fanout.a' is : " + message);
+        log.info("=======================================");
+    }
+}
