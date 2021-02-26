34e0d4699fe15cd51e2e926a5e3aa4119eef7471
kapcb <55942617+kapbc@users.noreply.github.com>
2021-02-26 12:49:30 +0800

    Update FanoutRabbitReceiverB.java

--- [096]Spring-Empty-Demo1/src/main/java/com/kapcb/ccc/service/rabbit/fanout/FanoutRabbitReceiverB.java
+++ [096]Spring-Empty-Demo1/src/main/java/com/kapcb/ccc/service/rabbit/fanout/FanoutRabbitReceiverB.java
@@ -25,7 +25,7 @@ public class FanoutRabbitReceiverB {
     public void processFanoutMessagesRabbitReceiveMessage(Map message) {
         log.info("=======This is Fanout B Exchange======");
         log.info("receive message from rabbit");
-        log.info("The message received from route key 'globalsource.fanout.b' is : " + message);
+        log.info("The message received from route key is : " + message);
         log.info("=======================================");
     }
 }
