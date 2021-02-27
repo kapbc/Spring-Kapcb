4fde7302884022711d0d019d1afb0058dbbb9f08
Eircc <2530628867@qq.com>
2021-02-27 23:52:04 +0800

    Spring-Refactory

--- [054]Spring-MVC-FileUpload/src/main/java/com/kapcb/ccc/controller/SpringMvcFileUploadController.java
+++ [054]Spring-MVC-FileUpload/src/main/java/com/kapcb/ccc/controller/SpringMvcFileUploadController.java
@@ -1,5 +1,17 @@
 package com.kapcb.ccc.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
 /**
  * <a>Title: SpringMvcFileUploadController </a>
  * <a>Author: kapcb <a>
@@ -9,5 +21,36 @@ package com.kapcb.ccc.controller;
  * @version 1.0.0
  * @date 2021/2/25 22:23
  */
+@Controller
+@RequestMapping("springmvc")
 public class SpringMvcFileUploadController {
+
+    private static final Logger log = LoggerFactory.getLogger(SpringMvcFileUploadController.class);
+
+    @RequestMapping(value = "UpLoad", method = RequestMethod.POST)
+    public String springMvcUpLoad(HttpServletRequest request, MultipartFile upload) throws IOException {
+
+        System.out.println("SpringMVC的传统上传方式");
+        //获取文件上传的目录
+        String realPath = request.getSession().getServletContext().getRealPath("/uploads");
+        //创建 file 对象
+        File file = new File(realPath);
+
+        //判断路径是否存在,如果不存在则创建文件
+        if (!file.exists()) {
+            file.mkdirs();
+        }
+
+        //获取上传文件的名字
+        String fileName = upload.getOriginalFilename();
+        String fileSuffix = fileName.substring(fileName.lastIndexOf("."));
+        String uuid = UUID.randomUUID().toString().toLowerCase();
+
+        File uploadFile = new File(file, uuid + fileSuffix);
+
+        //上传文件
+        upload.transferTo(uploadFile);
+        return "success";
+    }
+
 }
---
+++ [054]Spring-MVC-FileUpload/src/main/webapp/WEB-INF/template/success.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: ccc
+  Date: 2020/6/8
+  Time: 16:40
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<h1>成功</h1>
+
+</body>
+</html>
