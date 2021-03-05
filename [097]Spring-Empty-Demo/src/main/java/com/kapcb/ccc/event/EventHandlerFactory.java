e48be39c2c1616706077190677e308d76fe48dd9
kapcb <55942617+kapbc@users.noreply.github.com>
2021-03-05 16:21:31 +0800

    Create EventHandlerFactory.java

---
+++ [097]Spring-Empty-Demo/src/main/java/com/kapcb/ccc/event/EventHandlerFactory.java
@@ -0,0 +1,46 @@
+import com.alibaba.otter.canal.protocol.CanalEntry;
+import com.globalsources.cannal.service.IEventHandler;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * <a>Title: EventHandlerFactory </a>
+ * <a>Author: Mike Chen <a>
+ * <a>Description：<a>
+ *
+ * @author Mike Chen
+ * @date 2021/3/5-15:25
+ */
+@Slf4j
+public class EventHandlerFactory {
+
+    private static final int INITIAL_CAPACITY = 16;
+    private static final String COMMON_UNION_SPILT = "-";
+
+    private static final Map<String, IEventHandler> EVENT_HANDLER_MAP = new HashMap<>(INITIAL_CAPACITY);
+
+    public void setEventHandler(String key, IEventHandler iEventHandler) {
+        EVENT_HANDLER_MAP.put(key, iEventHandler);
+    }
+
+    public IEventHandler getEventHandler(String key) {
+        return EVENT_HANDLER_MAP.getOrDefault(key, null);
+    }
+
+    public static String createUnionKey(TableInfo tableInfo, CanalEntry.EventType eventType) {
+        return createUnionKey(tableInfo.getDatabaseName(), tableInfo.getTableName(), eventType);
+    }
+
+    private static String createUnionKey(String databaseName, String tableName, CanalEntry.EventType eventType) {
+        log.info("begin to create union key");
+        StringBuilder unionKey = new StringBuilder();
+        unionKey.append(databaseName).append(COMMON_UNION_SPILT);
+        unionKey.append(tableName).append(COMMON_UNION_SPILT);
+        unionKey.append(eventType.getValueDescriptor().getName());
+        log.info("union key create success, the unionKey is : " + unionKey.toString());
+        return unionKey.toString();
+    }
+
+}
