e64680b88ef417848051bd98c9f5f9e449b5d73c
kapcb <55942617+kapbc@users.noreply.github.com>
2021-03-05 18:56:57 +0800

    Update RedisHandler.java

--- [097]Spring-Empty-Demo/src/main/java/com/kapcb/ccc/component/RedisHandler.java
+++ [097]Spring-Empty-Demo/src/main/java/com/kapcb/ccc/component/RedisHandler.java
@@ -14,18 +14,43 @@ public class RedisHandler {
         log.info("first time insert data into redis success, the data is : " + resultData);
     }
 
-    private void updateOrDeleteFromList(boolean update, boolean after,CanalEntry.RowData rowData, String updateColumn) {
-        if (hasKey()) {
-            List<Object> result = redisService.getList(canalRedisServerKey, 0L, -1L);
-            log.info("before update or delete operation's data is : " + result);
-            Map<String, String> parse = DataParser.parse(rowData, after);
-            String s = parse.get(updateColumn);
+    public void delete(CanalEntry.RowData rowData, String rowKey) {
+        if (StringUtils.isNotBlank(rowKey) && hasKey(rowKey)) {
+            Object beforeDeleteValue = redisService.getHash(canalRedisServerKey, rowKey);
+            redisService.deleteHash(canalRedisServerKey, rowKey);
+            log.info("delete the key : " + canalRedisServerKey + " of the rowKey : " + rowKey + " from redis success, the value before delete is : " + beforeDeleteValue);
+            return;
         }
+        log.info("the key is not exist in redis");
     }
 
-    private boolean hasKey() {
-        return redisService.hasKey(canalRedisServerKey);
+    public void update(CanalEntry.RowData rowData, String rowKey) {
+        if (StringUtils.isNotBlank(rowKey) && hasKey(rowKey)) {
+            log.info("the key is exist in the redis, delete the key of : " + canalRedisServerKey + " of the rowKey : " + rowKey);
+            Object beforeDeleteValue = redisService.getHash(canalRedisServerKey, rowKey);
+            log.info("the value before delete is : " + beforeDeleteValue);
+            redisService.deleteHash(canalRedisServerKey, rowKey);
+            log.info("delete the key : " + canalRedisServerKey + " of the rowKey : " + rowKey + " success");
+        }
+        Map<String, String> parse = DataParser.parse(rowData, true);
+        redisService.setMultipleHashString(rowKey, parse);
+        log.info("update the key : " + canalRedisServerKey + " of the rowKey : " + rowKey + redisService.getHash(canalRedisServerKey, rowKey));
     }
 
+    public void insert(CanalEntry.RowData rowData, String rowKey) {
+        if (StringUtils.isNotBlank(rowKey) && hasKey(rowKey)) {
+            log.info("the key : " + canalRedisServerKey + " of the rowKey : " + rowKey + " is exist in the redis service");
+            Object beforeInsertValue = redisService.getHash(canalRedisServerKey, rowKey);
+            log.info("the value before insert is : " + beforeInsertValue);
+            redisService.deleteHash(canalRedisServerKey, rowKey);
+            log.info("delete the key : " + canalRedisServerKey + " of the rowKey : " + rowKey + " success");
+        }
+        Map<String, String> parse = DataParser.parse(rowData, true);
+        redisService.setMultipleHashString(rowKey, parse);
+        log.info("insert the key : " + canalRedisServerKey + " of the rowKey : " + rowKey + redisService.getHash(canalRedisServerKey, rowKey));
+    }
 
+    private boolean hasKey(String rowKey) {
+        return redisService.hasHashKey(canalRedisServerKey, rowKey);
+    }
 }
