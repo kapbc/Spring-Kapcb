16141c00cd5153305e870dbc03036506400d8411
kapcb <55942617+kapbc@users.noreply.github.com>
2021-03-09 12:27:07 +0800

    Create RoundRobinFactory.java

---
+++ [095]Spring-Empty-Demo2/src/main/java/com/kapcb/ccc/balance/robin/factory/RoundRobinFactory.java
@@ -0,0 +1,54 @@
+package com.kapcb.ccc.balance.robin.factory;
+
+import com.kapcb.ccc.balance.Invoker;
+import com.kapcb.ccc.balance.robin.RoundRobin;
+import com.kapcb.ccc.balance.robin.impl.WeightedRoundRobin;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+/**
+ * <a>Title: RoundRobinFactory </a>
+ * <a>Author: Mike Chen <a>
+ * <a>Description：<a>
+ *
+ * @author Mike Chen
+ * @date 2021/3/8-19:06
+ */
+@Slf4j
+public class RoundRobinFactory {
+
+    /**
+     * 正常
+     */
+    public static final String NORMAL = "normal";
+
+    /**
+     * 权重
+     */
+    public static final String WEIGHTED = "weighted";
+
+    public static RoundRobin create(String type, final Collection<Properties> properties) {
+        switch (type) {
+            case WEIGHTED:
+                Map<Invoker, Integer> invokerMap = new HashMap<>(properties.size());
+                properties.stream().forEach(element->{
+                    invokerMap.put(new Invoker() {
+                        @Override
+                        public Boolean isAvailable() {
+                            return Boolean.valueOf(element.getProperty("mail.isAvailable"));
+                        }
+
+                        @Override
+                        public String id() {
+                            return element.getProperty("mail.id");
+                        }
+                    },Integer.valueOf(element.getProperty("mail.weight")));
+                });
+                return new WeightedRoundRobin(invokerMap);
+        }
+    }
+}
