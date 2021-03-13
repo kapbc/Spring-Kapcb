f6bb3d90e8dedb8001ac0a4459e48f1284eb2433
kapcb <eircccallroot@yeah.net>
2021-03-13 23:12:12 +0800

    Spring-Data-MyBatis-Update

--- [068]Spring-Data-MyBatis-Update/src/main/java/com/kapcb/ccc/commons/ApplicationWebConfiguration.java
+++ [068]Spring-Data-MyBatis-Update/src/main/java/com/kapcb/ccc/commons/ApplicationWebConfiguration.java
@@ -4,15 +4,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 
 /**
- * <a>Title: ApplicationContextConfiguration </a>
+ * <a>Title: ApplicationWebConfiguration </a>
  * <a>Author: kapcb <a>
  * <a>Description: <a>
  *
  * @author kapcb
  * @version 1.0.0
- * @date 2021/3/13 23:03
+ * @date 2021/3/13 23:11
  */
 @Slf4j
 @Configuration
-public class ApplicationContextConfiguration {
+public class ApplicationWebConfiguration {
 }
