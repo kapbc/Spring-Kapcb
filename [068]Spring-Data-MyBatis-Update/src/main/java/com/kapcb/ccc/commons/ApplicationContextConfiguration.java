cf884df37dc3026a10c8ed16abd167dbed48dc2c
kapcb <eircccallroot@yeah.net>
2021-03-13 23:04:17 +0800

    Spring-Data-Mybatis-Update

---
+++ [068]Spring-Data-MyBatis-Update/src/main/java/com/kapcb/ccc/commons/ApplicationContextConfiguration.java
@@ -0,0 +1,13 @@
+package com.kapcb.ccc.commons;
+
+/**
+ * <a>Title: ApplicationContextConfiguration </a>
+ * <a>Author: kapcb <a>
+ * <a>Description: <a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2021/3/13 23:03
+ */
+public class ApplicationContextConfiguration {
+}
