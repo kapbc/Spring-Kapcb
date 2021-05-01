b1ce46ea5bc47c5ddf243cb1e3afc4578e8dca82
kapcb <eircccallroot@yeah.net>
2021-05-01 21:44:54 +0800

    Spring-AMQP-Rabbit-XML

--- [081]Spring-AMQP-RabbitMQ-Producter-XML/src/main/java/com/kapcb/ccc/commons/conditional/RabbitMqMessageConverterCondition.java
+++ [081]Spring-AMQP-RabbitMQ-Producter-XML/src/main/java/com/kapcb/ccc/commons/conditional/RabbitMqMessageConverterCondition.java
@@ -1,11 +1,14 @@
 package com.kapcb.ccc.commons.conditional;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Condition;
 import org.springframework.context.annotation.ConditionContext;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.core.type.AnnotatedTypeMetadata;
 import org.springframework.stereotype.Component;
+
+import java.util.Objects;
 
 /**
  * <a>Title: Spring-Kapcb </a>
@@ -20,11 +23,9 @@ import org.springframework.stereotype.Component;
 @Component
 public class RabbitMqMessageConverterCondition implements Condition {
 
+    @Override
     public boolean matches(ConditionContext conditionContext, AnnotatedTypeMetadata annotatedTypeMetadata) {
-        
-
-        return false;
-
+        MessageConverter bean = Objects.requireNonNull(conditionContext.getBeanFactory()).getBean(MessageConverter.class);
+        return Objects.nonNull(bean);
     }
-
 }
--- [081]Spring-AMQP-RabbitMQ-Producter-XML/src/main/java/com/kapcb/ccc/commons/config/RabbitConfiguration.java
+++ [081]Spring-AMQP-RabbitMQ-Producter-XML/src/main/java/com/kapcb/ccc/commons/config/RabbitConfiguration.java
@@ -2,12 +2,19 @@ package com.kapcb.ccc.commons.config;
 
 import com.kapcb.ccc.commons.conditional.RabbitMqMessageConverterCondition;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.connection.CorrelationData;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.util.concurrent.SettableListenableFuture;
+
+import java.util.Objects;
 
 /**
  * <a>Title: Spring-Kapcb </a>
@@ -23,12 +30,53 @@ import org.springframework.context.annotation.Configuration;
 public class RabbitConfiguration {
 
     @Bean
-    public MessageConverter messageConverter(){
+    public MessageConverter messageConverter() {
         return new Jackson2JsonMessageConverter();
     }
 
+    @Bean("rabbitTemplate")
+    @Scope(value = "prototype")
     @Conditional(value = {RabbitMqMessageConverterCondition.class})
-    public RabbitTemplate rabbitTemplate(){
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate();
+        rabbitTemplate.setConnectionFactory(connectionFactory);
+        rabbitTemplate.setMandatory(true);
 
+        /**
+         * 手动ack配置
+         */
+        rabbitTemplate.setConfirmCallback(((correlationData, ack, cause) -> {
+            if (ack) {
+                if (Objects.nonNull(correlationData)) {
+                    SettableListenableFuture<CorrelationData.Confirm> future = correlationData.getFuture();
+                    Message returnedMessage = correlationData.getReturnedMessage();
+                    String id = correlationData.getId();
+                    log.info("the correlationData's id is : " + id);
+                    log.info("the correlationData's future is : " + future);
+                    log.info("the correlationData's returnedMessage is : " + returnedMessage);
+                }
+            } else {
+                /**
+                 * 消费者消息接收失败，进行补偿机制
+                 * 失败消息存库，待后续重新发送
+                 */
+            }
+        }));
+
+        /**
+         * 消息发送失败
+         * 失败时按业务 选择从新投递或者丢弃
+         */
+        rabbitTemplate.setReturnCallback((message, replyCode, replyText, exchange, routingKey) -> {
+            log.info("RabbitReturnCallback        message : " + message);
+            log.info("RabbitReturnCallback        replyCode : " + replyCode);
+            log.info("RabbitReturnCallback        replyText : " + replyText);
+            log.info("RabbitReturnCallback        exchange : " + exchange);
+            log.info("RabbitReturnCallback        routingKey : " + routingKey);
+            log.info("the message consumer have received the message!");
+        });
+
+        rabbitTemplate.setMessageConverter(messageConverter());
+        return rabbitTemplate;
     }
 }
