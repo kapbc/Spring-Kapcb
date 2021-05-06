f281ab64507ba1e5f341cc69807ca11cf01fae5c
kapcb <eircccallroot@yeah.net>
2021-05-06 18:17:27 +0800

    Update RedisConfiguration.java

--- [055]Spring-Redis/src/main/java/com/kapcb/ccc/commons/configure/RedisConfiguration.java
+++ [055]Spring-Redis/src/main/java/com/kapcb/ccc/commons/configure/RedisConfiguration.java
@@ -109,14 +109,46 @@ public class RedisConfiguration {
         return jedisPoolConfig;
     }
     
-    @Bean
-    public JedisConnectionFactory jedisConnectionFactory(){
-        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
-        redisStandaloneConfiguration.setPort(1);
-        redisStandaloneConfiguration.setHostName("");
-        redisStandaloneConfiguration.setDatabase(0);
-        redisStandaloneConfiguration.setPassword("");
-        return new JedisConnectionFactory(redisStandaloneConfiguration);
-    }
+//     @Bean
+//     public JedisConnectionFactory jedisConnectionFactory(){
+//         RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+//         redisStandaloneConfiguration.setPort(1);
+//         redisStandaloneConfiguration.setHostName("");
+//         redisStandaloneConfiguration.setDatabase(0);
+//         redisStandaloneConfiguration.setPassword("");
+//         return new JedisConnectionFactory(redisStandaloneConfiguration);
+//     }
+    
+//         @Bean(value = "redisTemplate")
+//     public RedisTemplate<String, Object> redisTemplate(LettuceConnectionFactory lettuceConnectionFactory) {
+//         log.info("begin to create redisTemplate");
+//         lettuceConnectionFactory.setShareNativeConnection(false);
+//         RedisTemplate<String, Object> template = new RedisTemplate<>();
+
+//         Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
+
+//         ObjectMapper objectMapper = new ObjectMapper();
+//         PolymorphicTypeValidator ptv = BasicPolymorphicTypeValidator.builder().allowIfBaseType(Object.class).build();
+//         objectMapper.registerModule(new Jdk8Module());
+//         objectMapper.registerModule(new JavaTimeModule());
+//         objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+//         objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+//         objectMapper.activateDefaultTyping(ptv, ObjectMapper.DefaultTyping.NON_FINAL);
+//         jackson2JsonRedisSerializer.setObjectMapper(objectMapper);
+
+//         template.setKeySerializer(RedisSerializer.string());
+//         template.setHashKeySerializer(RedisSerializer.string());
+//         template.setValueSerializer(jackson2JsonRedisSerializer);
+//         template.setHashValueSerializer(jackson2JsonRedisSerializer);
+//         template.setConnectionFactory(lettuceConnectionFactory);
+//         template.afterPropertiesSet();
+//         return template;
+//     }
+
+//     @Bean
+//     @ConditionalOnBean(name = "redisTemplate")
+//     public RedisService redisService(@Qualifier("redisTemplate") RedisTemplate<String, Object> redisTemplate) {
+//         return new RedisService(redisTemplate);
+//     }
 
 }
